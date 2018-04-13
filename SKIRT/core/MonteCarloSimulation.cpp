ceaa73b46c8ca915a849fd094fe4c0e69281ca2e
Peter Camps <peter.camps@ugent.be>
2018-04-13 15:23:09 +0200

    add basic test code for big stored tables

--- SKIRT/core/MonteCarloSimulation.cpp
+++ SKIRT/core/MonteCarloSimulation.cpp
@@ -49,20 +49,31 @@ void MonteCarloSimulation::runSelf()
 {
     TimeLogger logger(log(), "the test phase");
 
-    log()->info("Located resource "+ FilePaths::resource("README.txt"));
-    log()->info("Located resource "+ FilePaths::resource("SunSED.stab"));
-
-    auto map = System::acquireMemoryMap(FilePaths::resource("SunSED.stab"));
-    auto map2 = System::acquireMemoryMap(FilePaths::resource("SunSED.stab"));
-    if (map != map2) log()->error("Mappings differ !");
-
-    auto start = static_cast<const char*>(map.first);
-    auto end = static_cast<const char*>(map.first) + map.second;
-
-    if (map.first)
+    for (int k = 1; k<=9; ++k)
     {
-        log()->warning(string(start, 7));
-        log()->warning(string(end-8, 7));
+        TimeLogger logger(log(), "big table " + std::to_string(k));
+
+        auto map = System::acquireMemoryMap(FilePaths::resource("BigTable"+std::to_string(k)+".stab"));
+        if (!map.first) throw FATALERROR("Cannot acquire memory map");
+        auto start = static_cast<const char*>(map.first);
+        auto end = static_cast<const char*>(map.first) + map.second;
+
+        log()->info(string(start, 7) + " ... " + string(end-8, 7));
+        size_t length = *(static_cast<const size_t*>(map.first) + 6);
+        log()->info("length: " + std::to_string(length));
+
+        const double* x = static_cast<const double*>(map.first) + 7;
+        const double* y = static_cast<const double*>(map.first) + 7 + length + 4;
+        log()->info("x: " + StringUtils::toString(x[1]) + "    y: " + StringUtils::toString(y[1]));
+
+        double xsum = 0;
+        double ysum = 0;
+        for (size_t i = 0; i<length; ++i)
+        {
+            xsum += x[i];
+            ysum += y[i];
+        }
+        log()->warning("xsum: " + StringUtils::toString(xsum) + "    ysum: " + StringUtils::toString(ysum));
     }
 }
 
