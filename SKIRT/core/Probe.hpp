4b06edb170bb34ba8e94251494f468eb8b31600b
Peter Camps <peter.camps@ugent.be>
2018-05-28 15:01:53 +0200

    add documentation to probe system

--- SKIRT/core/Probe.hpp
+++ SKIRT/core/Probe.hpp
@@ -10,7 +10,20 @@
 
 ////////////////////////////////////////////////////////////////////
 
-/** Probe is an abstract class representing probes that output stuff. */
+/** Probe is an abstract class representing probes that output information on internal simulation
+    data before or during the simulation run. Refer to the ProbeSystem class for more information.
+
+    Probe subclasses \em must adhere to the following rules. In this discussion, \em target refers
+    to the item(s) in the simulation hierarchy from which the probe retrieves information.
+    - Data encapsulation: a probe can use public interfaces only, even if these interfaces are in
+      some cases written specifically to support the probe (refer to the scenarios described in
+      the ProbeSystem class).
+    - Read-only access: a probe cannot change the data structures held by the target in any way.
+      As a corollary, during its own setup, a probe cannot cause setup of a target (it is allowed
+      to get and cache pointers to items, as long as they are not caused to perform setup).
+    - Interprobe independence: a probe cannot look for or depend on another probe, nor on the order
+      of the various probes in the list held by the probe system.
+*/
 class Probe : public SimulationItem
 {
     ITEM_ABSTRACT(Probe, SimulationItem, "a probe")
@@ -25,10 +38,16 @@ public:
         probes. */
     string itemName() const override;
 
-    /** This function performs probing after setup. */
+    /** This function is called at the end of the setup phase, i.e. after all simulation items have
+        performed setup. It is intended to output relevant information as requested by the user
+        configuration. The implementation in this base class does nothing. Each Probe subclass has
+        the opportunity to override this function and output something useful. */
     virtual void probeSetup();
 
-    /** This function performs probing after the simulation. */
+    /** This function is called at the end of the run phase, i.e. after all photon packets have
+        been emitted and detected. It is intended to output relevant information as requested by
+        the user configuration. The implementation in this base class does nothing. Each Probe
+        subclass has the opportunity to override this function and output something useful. */
     virtual void probeRun();
 };
 
--- SKIRT/core/ProbeSystem.hpp
+++ SKIRT/core/ProbeSystem.hpp
@@ -10,24 +10,71 @@
 
 //////////////////////////////////////////////////////////////////////
 
-/** A ProbeSystem instance maintains a list of zero or more probes and helps ensure that these probes are
-invoked at the appropriate times. */
+/** A ProbeSystem instance maintains a list of zero or more probes (instances of Probe subclasses)
+    and helps ensure that these probes are invoked at the appropriate times.
+
+    SKIRT essentially writes two kinds of output. A first type of output can be called "mock
+    observations", i.e. the fluxes and surface densities obtained by detecting photon packets
+    launched (or peeled-off) during the simulation. This output is written by the Instrument
+    subclasses at the end of the simulation run, and it is often the main simulation result. A
+    second type of output is information on any of the data structures constructed in preparation
+    for or during the simulation run, i.e. internal data. This includes diagnostics used to verify
+    configuration and operation of the code and physical quantities that are computed by the
+    simulation but cannot be “observed” from the outside. The goal of the probe system is to
+    move responsibility for producing the second type of output to a separate set of
+    classes/objects as opposed to embedding it in the simulation code itself.
+
+    This design has a number of benefits. From a user perspective, the probe objects can take
+    additional attributes to customize the output; for example, requesting a cut through the medium
+    density at some offset from the coordinate plane. The probe list can also contain multiple
+    probes of the same kind; for example, requesting a cut through the medium density at two or
+    more offsets from the coordinate plane. From a developer perspective, there is a more explicit
+    interface between the simulation and output code portions, resulting in improved data
+    encapsulation. The simulation code is not cluttered with output code, and new probe types can
+    be implemented without changing the simulation code (at least, in most cases; see below).
+
+    There are essentially two scenarios for probes to retrieve information from item(s) in the
+    simulation hierarchy (the \em target). In the preferred scenario, a probe uses public members
+    of the target to retrieve data, and implements the output procedure in the probe. This might
+    involve adding some getters to target classes specifically for supporting probe(s). In more
+    involved situations, the data can be prepared for output in a public function of the target
+    class, while still performing the actual output in the probe, possibly through a callback
+    mechanism. This scenario is less preferable because it breaks the clean separation between
+    simulation and output code. However, it can be the appropriate choice when the output is best
+    implemented through a hierarchy of virtual functions. In any case, probes cannot change the
+    data structures held by the target.
+
+    Each Monte Carlo simulation hierarchy has a single ProbeSystem instance, which holds a list
+    of Probe subclasses configured by the user. The probeXXX() functions of the probe system,
+    and thereby the corresponding probeXXX() functions in each probe object are called at the
+    following times during a simulation:
+
+    Name  | Called at the end of
+    ------|----------------------
+    probeSetup() | the setup phase, i.e. after all simulation items have performed setup
+    probeRun()   | the run phase, i.e. after all photon packets have been emitted and detected
+
+    */
 class ProbeSystem : public SimulationItem
 {
     ITEM_CONCRETE(ProbeSystem, SimulationItem, "a probe system")
 
     PROPERTY_ITEM_LIST(probes, Probe, "the probes")
-        ATTRIBUTE_OPTIONAL(instruments)
+        ATTRIBUTE_OPTIONAL(probes)
 
     ITEM_END()
 
     //======================== Other Functions =======================
 
 public:
-    /** This function invokes the function of the same name on all probes in the probe system. */
+    /** This function is called at the end of the setup phase, i.e. after all simulation items have
+        performed setup. It invokes the function of the same name on all probes in the probe
+        system. */
     void probeSetup();
 
-    /** This function invokes the function of the same name on all probes in the probe system. */
+    /** This function is called at the end of the run phase, i.e. after all photon packets have
+        been emitted and detected. It invokes the function of the same name on all probes in the
+        probe system. */
     void probeRun();
 };
 
