05cadfeaa0532fcb4643d5679aa3362d1b051648
Peter Camps <peter.camps@ugent.be>
2018-07-04 21:37:57 +0200

    add parameter clamping to castelli kurucz sed family

--- SKIRT/core/CastelliKuruczSEDFamily.cpp
+++ SKIRT/core/CastelliKuruczSEDFamily.cpp
@@ -36,6 +36,30 @@ vector<SnapshotParameter> CastelliKuruczSEDFamily::parameterInfo() const
 
 ////////////////////////////////////////////////////////////////////
 
+namespace
+{
+    // force the gravity value inside the valid portion of the grid depending on the temperature
+    void clampParameterValues(double& T, double& g)
+    {
+        // cutoff values for temperature and gravity (see table in class documentation)
+        static Array Tv = { 49000, 39000, 31000, 26000, 19000, 11750, 9000, 8250, 7500, 6000 };
+        static Array gv = pow(10, Array({ 5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5 })-2);
+        static size_t n = Tv.size();
+
+        // clamp the gravity value if needed
+        for (size_t i=0; i!=n; ++i)
+        {
+            if (T > Tv[i] && g < gv[i])
+            {
+                g = gv[i];
+                break;
+            }
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+
 double CastelliKuruczSEDFamily::specificLuminosity(double wavelength, const Array& parameters) const
 {
     double R = parameters[0];
@@ -44,6 +68,7 @@ double CastelliKuruczSEDFamily::specificLuminosity(double wavelength, const Arra
     double g = parameters[3];
 
     // if needed, force the parameter values inside the valid portion of the grid
+    clampParameterValues(T, g);
 
     return 4.*M_PI * R*R * _table(wavelength, Z, T, g);
 }
@@ -59,6 +84,7 @@ double CastelliKuruczSEDFamily::cdf(Array& lambdav, Array& pv, Array& Pv,
     double g = parameters[3];
 
     // if needed, force the parameter values inside the valid portion of the grid
+    clampParameterValues(T, g);
 
     return 4.*M_PI * R*R * _table.cdf(lambdav, pv, Pv, wavelengthRange, Z, T, g);
 }
