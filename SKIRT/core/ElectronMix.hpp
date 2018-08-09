5b96a1520990bff53de01e438b490be10a8332c4
Peter Camps <peter.camps@ugent.be>
2018-08-09 21:30:49 +0200

    add documentation to single-grain mix and electron mix

--- SKIRT/core/ElectronMix.hpp
+++ SKIRT/core/ElectronMix.hpp
@@ -19,7 +19,10 @@
     The scattering cross section is given by the well-known Thomson cross section (a constant) and
     the Mueller matrix elements (and hence the phase function) can be expressed analytically as a
     function of just the scattering angle; see Bohren & Huffman (1998) or Wolf 2003 (Computer
-    Physics Communications, 150, 99–115). */
+    Physics Communications, 150, 99–115).
+
+    The implementation of this class is based on the analysis presented by Peest at al. 2017 (A&A,
+    601, A92). */
 class ElectronMix : public MaterialMix
 {
     ITEM_CONCRETE(ElectronMix, MaterialMix, "a population of electrons")
@@ -79,11 +82,22 @@ public:
     /** This function returns the value of the scattering phase function
         \f$\Phi_\lambda(\cos\theta)\f$ at wavelength \f$\lambda\f$ for the specified scattering
         angle cosine \f$\cos\theta\f$, where the phase function is normalized as \f[\int_{-1}^1
-        \Phi_\lambda(\cos\theta) \,\mathrm{d}\cos\theta =2.\f] */
+        \Phi_\lambda(\cos\theta) \,\mathrm{d}\cos\theta =2.\f]
+
+        The phase function for unpolarized radiation is obtained from the general polarized case
+        described for the phaseFunctionValue() function by setting the linear polarization degree
+        \f$P_\text{L}\f$ to zero, which yields simply \f[ \Phi(\theta) =
+        \frac{3}{4}\,(\cos^2\theta+1). \f] */
     double phaseFunctionValueForCosine(double lambda, double costheta) const override;
 
     /** This function generates a random scattering angle cosine sampled from the phase function
-        \f$\Phi_\lambda(\cos\theta)\f$ at wavelength \f$\lambda\f$. */
+        \f$\Phi_\lambda(\cos\theta)\f$ at wavelength \f$\lambda\f$.
+
+        The phase function for unpolarized radiation is obtained from the general polarized case
+        described for the phaseFunctionValue() function by setting the linear polarization degree
+        \f$P_\text{L}\f$ to zero, which yields \f[ \Phi(\theta) \propto \cos^2\theta+1. \f] We can
+        thus use the same procedure for sampling \f$\theta\f$ as described for the
+        phaseFunctionValue() function. */
     double generateCosineFromPhaseFunction(double lambda) const override;
 
     //======== Polarization through scattering by spherical particles =======
@@ -93,18 +107,51 @@ public:
         \f$\Phi_\lambda(\theta,\phi)\f$ at wavelength \f$\lambda\f$ for the specified scattering
         angles \f$\theta\f$ and \f$\phi\f$, and for the specified incoming polarization state. The
         phase function is normalized as \f[\int\Phi_\lambda(\theta,\phi) \,\mathrm{d}\Omega
-        =4\pi.\f] */
+        =4\pi.\f]
+
+        For Thomson scattering, we can substitute the analytical expressions for the Mueller
+        coefficients into the phase function, which leads to \f[ \Phi(\theta,\phi) =
+        \frac{3}{4}\,\left[(\cos^2\theta+1) + P_{\text{L}}\,(\cos^2\theta-1)\cos2(\phi - \gamma)
+        \right], \f] where \f$P_\text{L}\f$ is the linear polarization degree and \f$\gamma\f$ the
+        polarization angle of the incoming photon, and there is no wavelength dependence. */
    double phaseFunctionValue(double lambda, double theta, double phi, const StokesVector* sv) const override;
 
     /** This function generates random scattering angles \f$\theta\f$ and \f$\phi\f$ sampled from
         the phase function \f$\Phi_\lambda(\theta,\phi)\f$ at wavelength \f$\lambda\f$, and for the
         specified incoming polarization state. The results are returned as a pair of numbers in the
-        order \f$\theta\f$ and \f$\phi\f$. */
+        order \f$\theta\f$ and \f$\phi\f$.
+
+        For Thomson scattering, we sample from the phase function listed for the
+        phaseFunctionValue() function using the conditional probability technique. We reduce the
+        phase function to the marginal distribution by integrating over \f$\phi\f$, yielding \f[
+        \Phi(\theta) \propto \cos^2\theta+1. \f] With the substitution \f$t = \cos\theta\f$, the
+        normalized probability distribution to be sampled can be written as \f[ p(t) =
+        \frac{3}{8}\,(t^2+1),\text{ with } -1<t<1. \f] The corresponding cumulative distribution is
+        \f[ P(t) = \int_{-1}^t \frac{3}{8}\,(t'^2+1)\,\text{d}t' = \frac{1}{8}\,(t^3+3t+4). \f] The
+        equation \f${\cal{X}} = P(t)\f$, with \f${\cal{X}}\f$ a uniform deviate, has a single
+        real-valued solution, \f[ t = \frac{p^{2/3}-1}{p^{1/3}} \quad\text{with}\quad p =
+        4{\cal{X}}-2 +\sqrt{16{\cal{X}}^2 -16{\cal{X}} +5}\f]
+
+        Once we have selected a random scattering angle \f$\theta=\arccos(t)\f$, we sample a random
+        azimuthal angle \f$\phi\f$ from the normalized conditional distribution, \f[
+        \Phi_\theta(\phi) =\frac{\Phi(\theta,\phi)}{\int_0^{2\pi}
+        \Phi(\theta,\phi')\,\text{d}\phi'} =\frac{1}{2\pi}\left(1+
+        P_{\text{L}}\,\frac{\cos^2\theta-1}{\cos^2\theta+1}\cos 2(\phi - \gamma)\right). \f]
+
+        This can again be done through numerical inversion, by solving the equation \f[ {\cal{X}}
+        =\int_{0}^{\phi}\Phi_{\theta}(\phi')\,\text{d}\phi' =\frac{1}{2\pi} \left( \phi +
+        P_{\text{L}}\,\frac{\cos^2\theta-1}{\cos^2\theta+1} \sin\phi \cos(\phi - 2\gamma)\right)
+        \f] for \f$\phi\f$, with \f${\cal{X}}\f$ being a new uniform deviate. */
     std::pair<double,double> generateAnglesFromPhaseFunction(double lambda, const StokesVector* sv) const override;
 
     /** This function applies the Mueller matrix transformation for the specified wavelength
         \f$\lambda\f$ and scattering angle \f$\theta\f$ to the given polarization state (which
-        serves as both input and output for the function). */
+        serves as both input and output for the function).
+
+        For scattering by spherical particles, the Mueller matrix has only four independent
+        coefficients. For Thomson scattering, these can be analytically expressed as
+        \f[S_{11}=\cos^2\theta+1,\quad S_{12}=\cos^2\theta-1,\quad S_{33}=2\cos\theta,\quad \text{
+        and } S_{34}=0,\f] i.e. without dependence on wavelength. */
     void applyMueller(double lambda, double theta, StokesVector* sv) const override;
 
     //======================== Data Members ========================
--- SKIRT/core/SingleGrainDustMix.hpp
+++ SKIRT/core/SingleGrainDustMix.hpp
@@ -15,7 +15,10 @@
     representative grain, with or without support for polarization by scattering. This base class
     includes the implementations of the required functions. Subclasses must merely provide the
     names of the relevant resource files and implement the scatteringMode() function (the
-    implementation in this class auto-adapts to the returned value). */
+    implementation in this class auto-adapts to the returned value).
+
+    The implementation of this class is based on the analysis presented by Peest at al. 2017 (A&A,
+    601, A92). */
 class SingleGrainDustMix : public MaterialMix
 {
     ITEM_ABSTRACT(SingleGrainDustMix, MaterialMix, "a dust mix described by a single representative grain")
@@ -37,11 +40,11 @@ protected:
     virtual string resourceNameForOpticalProps() const = 0;
 
     /** This function must be implemented in a subclass to return the name of the stored table
-        resource tabulating the elements of the Mueller matrix as a function of wavelength and
+        resource tabulating the coefficients of the Mueller matrix as a function of wavelength and
         scattering angle. This function is invoked for the MaterialPhaseFunction scattering mode
-        (to obtain S11) and for the SphericalPolarization (to obtain all four Sxx coefficients).
-        The default implementation in this base class returns the empty string, which is acceptable
-        only with the HenyeyGreenstein scattering mode. */
+        (to obtain \f$S_{11}\f$ ) and for the SphericalPolarization (to obtain all four
+        \f$S_{xx}\f$ coefficients). The default implementation in this base class returns the empty
+        string, which is acceptable only with the HenyeyGreenstein scattering mode. */
     virtual string resourceNameForMuellerMatrix() const;
 
     //======== Functionality levels =======
@@ -85,11 +88,23 @@ public:
     /** This function returns the value of the scattering phase function
         \f$\Phi_\lambda(\cos\theta)\f$ at wavelength \f$\lambda\f$ for the specified scattering
         angle cosine \f$\cos\theta\f$, where the phase function is normalized as \f[\int_{-1}^1
-        \Phi_\lambda(\cos\theta) \,\mathrm{d}\cos\theta =2.\f] */
+        \Phi_\lambda(\cos\theta) \,\mathrm{d}\cos\theta =2.\f]
+
+        The phase function for unpolarized radiation is obtained from the general polarized case
+        described for the phaseFunctionValue() function by setting the linear polarization degree
+        \f$P_\text{L}\f$ to zero. The simplified function uses only the first Mueller matrix
+        coefficient \f$S_{11}\f$. */
     virtual double phaseFunctionValueForCosine(double lambda, double costheta) const override;
 
     /** This function generates a random scattering angle cosine sampled from the phase function
-        \f$\Phi_\lambda(\cos\theta)\f$ at wavelength \f$\lambda\f$. */
+        \f$\Phi_\lambda(\cos\theta)\f$ at wavelength \f$\lambda\f$.
+
+        The phase function for unpolarized radiation is obtained from the general polarized case
+        described for the phaseFunctionValue() function by setting the linear polarization degree
+        \f$P_\text{L}\f$ to zero. The simplified function no longer depends on \f$\phi\f$, and uses
+        only the first Mueller matrix coefficient \f$S_{11}\f$. To sample the scattering angle
+        \f$\theta\f$ from this phase function, we follow the same procedure as described for the
+        generateAnglesFromPhaseFunction() function, with \f$P_\text{L}=0\f$. */
     virtual double generateCosineFromPhaseFunction(double lambda) const override;
 
     //======== Polarization through scattering by spherical particles =======
@@ -99,18 +114,54 @@ public:
         \f$\Phi_\lambda(\theta,\phi)\f$ at wavelength \f$\lambda\f$ for the specified scattering
         angles \f$\theta\f$ and \f$\phi\f$, and for the specified incoming polarization state. The
         phase function is normalized as \f[\int\Phi_\lambda(\theta,\phi) \,\mathrm{d}\Omega
-        =4\pi.\f] */
+        =4\pi.\f]
+
+        The phase function for scattering by spherical grains can be written as \f[
+        \Phi(\theta,\phi) = N\,S_{11} \left( 1 + P_{\text{L}}\,\frac{S_{12}}{S_{11}}\cos2(\phi -
+        \gamma) \right) \f] with \f[ N=\frac{1}{2\pi\int_0^\pi S_{11}\sin\theta\, \text{d}\theta},
+        \f] where \f$P_\text{L}\f$ is the linear polarization degree and \f$\gamma\f$ the
+        polarization angle of the incoming photon, and where the Mueller matrix coefficients
+        \f$S_{xx}\f$ depend on both the photon wavelength \f$\lambda\f$ and the scattering angle
+        \f$\theta\f$. As a result, the normalization constant cannot be pre-calculated. */
    double phaseFunctionValue(double lambda, double theta, double phi, const StokesVector* sv) const override;
 
     /** This function generates random scattering angles \f$\theta\f$ and \f$\phi\f$ sampled from
         the phase function \f$\Phi_\lambda(\theta,\phi)\f$ at wavelength \f$\lambda\f$, and for the
         specified incoming polarization state. The results are returned as a pair of numbers in the
-        order \f$\theta\f$ and \f$\phi\f$. */
+        order \f$\theta\f$ and \f$\phi\f$.
+
+        For scattering by spherical grains, we sample from the phase function listed for the
+        phaseFunctionValue() function using the conditional probability technique. We reduce the
+        phase function to the marginal distribution \f$\Phi(\theta)\f$, \f[ \Phi(\theta)
+        =\int_0^{2\pi} \Phi(\theta,\phi)\,\text{d}\phi =2\pi\ N\,S_{11} =\frac{S_{11}}{\int_0^\pi
+        S_{11}\sin\theta'\, \text{d}\theta'} \ . \f] We sample a random \f$\theta\f$ value from
+        this distribution through numerical inversion, that is to say, by solving the equation
+        \f[\label{eq:numInvTheta} {\cal{X}} =\frac{\int_0^\theta
+        S_{11}\sin\theta'\,\text{d}\theta'}{\int_0^\pi S_{11}\sin\theta'\, \text{d}\theta'} \f] for
+        \f$\theta\f$, where \f${\cal{X}}\f$ is a uniform deviate.
+
+        Once we have selected a random scattering angle \f$\theta\f$, we sample a random azimuthal
+        angle \f$\phi\f$ from the normalized conditional distribution, \f[ \Phi_\theta(\phi)
+        =\frac{\Phi(\theta,\phi)}{\int_0^{2\pi} \Phi(\theta,\phi')\,\text{d}\phi'}
+        =\frac{1}{2\pi}\left(1+ P_{\text{L}}\,\frac{S_{12}}{S_{11}}\cos 2(\phi - \gamma)\right),
+        \f] where the ratio \f$S_{12}/S_{11}\f$ depends on the scattering angle \f$\theta\f$ in
+        addition to wavelength.
+
+        This can again be done through numerical inversion, by solving the equation \f[ {\cal{X}}
+        =\int_{0}^{\phi}\Phi_{\theta}(\phi')\,\text{d}\phi' =\frac{1}{2\pi} \left( \phi +
+        P_{\text{L}}\,\frac{S_{12}}{S_{11}} \sin\phi \cos(\phi - 2\gamma)\right) \f] for
+        \f$\phi\f$, with \f${\cal{X}}\f$ being a new uniform deviate. */
     std::pair<double,double> generateAnglesFromPhaseFunction(double lambda, const StokesVector* sv) const override;
 
     /** This function applies the Mueller matrix transformation for the specified wavelength
         \f$\lambda\f$ and scattering angle \f$\theta\f$ to the given polarization state (which
-        serves as both input and output for the function). */
+        serves as both input and output for the function).
+
+        For scattering by spherical grains, the Mueller matrix has only four independent
+        coefficients, namely \f$S_{11}\f$, \f$S_{12}\f$, \f$S_{33}\f$, and \f$S_{34}\f$, which
+        depend on both the photon wavelength \f$\lambda\f$ and the scattering angle \f$\theta\f$.
+        These coefficients are obtained from the tables loaded during setup (and specified by name
+        in each subclass. */
     void applyMueller(double lambda, double theta, StokesVector* sv) const override;
 
     //======================== Data Members ========================
