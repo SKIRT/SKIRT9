2b927f172724494b90fa67444f5adb2729372d7a
Peter Camps <peter.camps@ugent.be>
2019-01-07 11:16:12 +0100

    fix inheritance declaration

--- SKIRT/core/TabulatedDustMix.hpp
+++ SKIRT/core/TabulatedDustMix.hpp
@@ -28,7 +28,7 @@
     The subclass must load the tabulated data, and this abstract class handles everything else. */
 class TabulatedDustMix : public DustMix
 {
-    ITEM_ABSTRACT(TabulatedDustMix, MaterialMix, "a basic dust mix with properties tabulated by the user")
+    ITEM_ABSTRACT(TabulatedDustMix, DustMix, "a basic dust mix with properties tabulated by the user")
         ATTRIBUTE_TYPE_ALLOWED_IF(TabulatedDustMix, "!StochasticDustEmission")
         ATTRIBUTE_TYPE_DISPLAYED_IF(TabulatedDustMix, "Level2")
     ITEM_END()
