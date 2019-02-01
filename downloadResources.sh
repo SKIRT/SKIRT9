1a9edf24a92dc91331af7fe76472e4455d88973d
Peter Camps <peter.camps@ugent.be>
2019-02-01 13:54:29 +0100

    add shell script for downloading resources

---
+++ downloadResources.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# (use "chmod +rx scriptname" to make script executable)
+#
+# For use on any Unix system, including Mac OS X and Linux
+#
+# Execute this script with "git" as default directory to download
+# the SKIRT 9 resource files provided on the public SKIRT server, and
+# place them in the 'resources' directory next to the git directory.
+#
+
+# list of archives to be downloaded and extracted (separate filenames with a space)
+FILELIST=( StoredTables.tar.gz )
+
+# download using wget (Linux) or curl (Mac OS X)
+if which wget >/dev/null
+then
+    DOWNLOAD=wget
+elif which curl >/dev/null
+then
+    DOWNLOAD="curl -O"
+else
+    echo error: no wget or curl available to download files
+    exit
+fi
+
+# download and extract each of the files in the list
+filecount=$((${#FILELIST[@]} - 1))
+for i in $(eval echo {0..$filecount})
+do
+    FILENAME=${FILELIST[$i]}
+    echo "----------------------------------------"
+    echo downloading $FILENAME ...
+    mkdir -p ../resources
+    cd ../resources
+    $DOWNLOAD http://sciences.ugent.be/skirtextdat/SKIRT9/Resources/$FILENAME
+    tar -xvf $FILENAME
+    rm $FILENAME
+    cd ../git
+done
+
+echo "----------------------------------------"
+echo Done.
