3413645f2ad810a9e480c727f9b992e155f582f7
Peter Camps <peter.camps@ugent.be>
2019-04-08 13:50:20 +0200

    update wavelength column header in emissivity probe

--- SKIRT/core/DustEmissivityProbe.cpp
+++ SKIRT/core/DustEmissivityProbe.cpp
@@ -94,7 +94,7 @@ namespace
 
         // write the header
         file.writeLine("# Dust emissivities for input field " + title);
-        file.addColumn("lambda", units->uwavelength());
+        file.addColumn("wavelength", units->uwavelength());
         for (int h : hv) file.addColumn("lambda*j_lambda for dust in medium component " + std::to_string(h), "W/sr/H");
 
         // write the emissivity for each dust mix to file
