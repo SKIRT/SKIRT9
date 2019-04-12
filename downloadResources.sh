0538a68d2fe731eef24e08f668fd0758991a21b5
Peter Camps <peter.camps@ugent.be>
2019-04-12 10:42:28 +0200

    resource download procedure uses external list of expected resources

--- downloadResources.sh
+++ downloadResources.sh
@@ -8,35 +8,67 @@
 # place them in the 'resources' directory next to the git directory.
 #
 
-# list of archives to be downloaded and extracted (separate filenames with a space)
-FILELIST=( StoredTables.tar.gz )
-
-# download using wget (Linux) or curl (Mac OS X)
+# select download command: wget (Linux) or curl (Mac OS X)
 if which wget >/dev/null
 then
-    DOWNLOAD="wget --no-check-certificate"
+    #DOWNLOAD="wget --no-check-certificate https://sciences.ugent.be/skirtextdat/SKIRT9/"
+    DOWNLOAD="wget http://www.skirt.ugent.be/skirtextdat/SKIRT9/"
 elif which curl >/dev/null
 then
-    DOWNLOAD="curl --insecure -O"
+    #DOWNLOAD="curl --insecure -O https://sciences.ugent.be/skirtextdat/SKIRT9/"
+    DOWNLOAD="curl -O http://www.skirt.ugent.be/skirtextdat/SKIRT9/"
 else
     echo error: no wget or curl available to download files
-    exit
+    exit 1
 fi
 
-# download and extract each of the files in the list
-filecount=$((${#FILELIST[@]} - 1))
-for i in $(eval echo {0..$filecount})
+# loop over the list of expected archives and versions given in the text file in the SKIRT repository
+while read -u 3 LINE        # use explicit file descriptor 3 to allow nested read from terminal
 do
-    FILENAME=${FILELIST[$i]}
-    echo "----------------------------------------"
-    echo downloading $FILENAME ...
-    mkdir -p ../resources
-    cd ../resources
-    $DOWNLOAD https://sciences.ugent.be/skirtextdat/SKIRT9/Resources/$FILENAME
-    tar -xvf $FILENAME
-    rm $FILENAME
-    cd ../git
-done
+    read NAME VERSION <<< $LINE
+    RESOURCENAME=SKIRT9_Resources_$NAME
+    VERSIONPATH=../resources/${RESOURCENAME}/version.txt
+    ZIPFILENAME=${RESOURCENAME}_v${VERSION}.zip
 
-echo "----------------------------------------"
+    # find out whether the expected version is already installed, and if not, ask the user what to do
+    PROCEED=0
+    if [ -e $VERSIONPATH ]
+    then
+        read INSTALLEDVERSION <<< $(<$VERSIONPATH)
+        if [ $INSTALLEDVERSION = $VERSION ]
+        then
+            echo $RESOURCENAME version $VERSION is already installed -- skipping
+        else
+            echo $RESOURCENAME version $INSTALLEDVERSION is installed while version $VERSION is expected
+            read -p "Do you want to download and install $RESOURCENAME version $VERSION? [y/n] " RESPONSE
+            if [[ $RESPONSE =~ ^[Yy] ]]
+            then
+                PROCEED=1
+            fi
+        fi
+    else
+        echo $RESOURCENAME is not installed
+        read -p "Do you want to download and install $RESOURCENAME version $VERSION? [y/n] " RESPONSE
+        if [[ $RESPONSE =~ ^[Yy] ]]
+        then
+            PROCEED=1
+        fi
+    fi
+
+    # if confirmed by the user, download the archive
+    if [ $PROCEED = 1 ]
+    then
+        echo Downloading $ZIPFILENAME ...
+        echo "------------------------------------------------"
+        mkdir -p ../resources
+        cd ../resources
+        $DOWNLOAD$ZIPFILENAME
+        unzip -o $ZIPFILENAME  # overwrite exisiting files
+        rm $ZIPFILENAME
+        cd ../git
+        echo "------------------------------------------------"
+    fi
+
+done 3< "SKIRT/resources/ExpectedResources.txt"
+
 echo Done.
