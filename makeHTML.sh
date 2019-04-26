42c5e9fb6d2a44a536e7d887cdf1dfef75e0a746
Peter Camps <peter.camps@ugent.be>
2019-04-26 13:35:21 +0200

    Build local html pages in subdirectory for ease of use

--- makeHTML.sh
+++ makeHTML.sh
@@ -5,4 +5,8 @@
 #
 
 # generate the html documentation in a folder next to the git folder
+mkdir -p ../html
 /Applications/Doxygen.app/Contents/Resources/doxygen Docs/doxygen/html.doxygen
+
+# copy redirecting index.html file
+cp Docs/doxygen/index_root.html ../html/index.html
