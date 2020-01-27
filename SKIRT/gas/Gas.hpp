97042d7a8ef7ca97e0dd2c2681c53e6f1679dfa7
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2020-01-27 15:27:54 +0100

    Do not exclude members using ifdef

--- SKIRT/gas/Gas.hpp
+++ SKIRT/gas/Gas.hpp
@@ -13,29 +13,10 @@ namespace GasModule
     class GasState;
 }
 
-#ifdef BUILD_WITH_GAS
-#    include <vector>
-#endif
+#include <vector>
 
 /** The stuff directly interfacing with the gas module should go into one or multiple classes of
-    this subproject, so that the code can easily be built without the gas module. Things to think
-    about:
-
-    - Whether to store the gas states for every cell, or to pick and choose information from them
-      (temperature / opacity) and store that info in an easier to manage data structure.
-
-    - Updating gas states (calling the interface)
-
-    - Exctracting information from gas states
-
-    - Passing the wavelength grid at setup
-
-    - Passing the grain properties when updating gas states
-
-    - Multiprocessing
-
-    - Should anything be integrated with the MaterialMix, or maybe the future 'constituent' concept?
-*/
+    this subproject, so that the code can easily be built without the gas module. */
 class Gas
 {
 public:
@@ -45,11 +26,9 @@ public:
     void allocateGasStates(size_t num);
     void updateGasState(int m, const Array& meanIntensity);
     double gasTemperature(int m) const;
-#ifdef BUILD_WITH_GAS
 private:
     std::vector<GasModule::GasState> _statev;
     std::unique_ptr<GasModule::GasInterface> _gi;
-#endif
 };
 
 #endif
