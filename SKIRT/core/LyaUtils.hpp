5dfeaf3544559d994a9b3e77b143242e7306360d
Peter Camps <peter.camps@ugent.be>
2020-04-10 09:45:41 +0200

    Add documentation to Lya utils

--- SKIRT/core/LyaUtils.hpp
+++ SKIRT/core/LyaUtils.hpp
@@ -21,6 +21,8 @@ class Random;
     of the subsequent downward transition. This happens fast enough that we can consider the
     combined process as a scattering event.
 
+    <b>Scattering cross section</b>
+
     The cross section for a single hydrogen atom of the Lya scattering of a photon can be derived
     using quantum mechanical considerations, resulting in a sharply peaked profile as a function of
     the photon wavelength in the atom's rest frame. Because each atom has its own velocity, a
@@ -34,15 +36,13 @@ class Random;
     T}{m_\mathrm{p}}} \f] where \f$k_\mathrm{B}\f$ is the Boltzmann constant, \f$m_\mathrm{p}\f$ is
     the proton mass, and \f$T\f$ is the temperature of the gas. We then introduce the dimensionless
     frequency variable \f$x\f$, defined as \f[ x = \frac{\nu - \nu_\alpha}{\nu_\alpha}
-    \,\frac{c}{v_\mathrm{th}} = \frac{v_\mathrm{p}}{v_\mathrm{th}} \f]
-
-    where \f$\nu=c/\lambda\f$ is the regular frequency variable, \f$\nu_\alpha=c/\lambda_\alpha\f$
-    is the frequency at the Lya line center, \f$\lambda_\alpha\f$ is the wavelength at the Lya line
-    center, and \f$c\f$ is the speed of light in vacuum. The last equality introduces the velocity
-    shift \f$v_\mathrm{p}\f$ of the photon frequency relative to the Lya line center, defined by
-    \f[ \frac{v_\mathrm{p}}{c} = \frac{\nu - \nu_\alpha}{\nu_\alpha} \approx -\frac{\lambda -
-    \lambda_\alpha}{\lambda_\alpha} \f] where the approximate equality holds for \f$v_\mathrm{p}\ll
-    c\f$.
+    \,\frac{c}{v_\mathrm{th}} = \frac{v_\mathrm{p}}{v_\mathrm{th}} \f] where \f$\nu=c/\lambda\f$ is
+    the regular frequency variable, \f$\nu_\alpha=c/\lambda_\alpha\f$ is the frequency at the Lya
+    line center, \f$\lambda_\alpha\f$ is the wavelength at the Lya line center, and \f$c\f$ is the
+    speed of light in vacuum. The last equality introduces the velocity shift \f$v_\mathrm{p}\f$ of
+    the photon frequency relative to the Lya line center, defined by \f[ \frac{v_\mathrm{p}}{c} =
+    \frac{\nu - \nu_\alpha}{\nu_\alpha} \approx -\frac{\lambda - \lambda_\alpha}{\lambda_\alpha}
+    \f] where the approximate equality holds for \f$v_\mathrm{p}\ll c\f$.
 
     After neglecting some higher order terms, the convolution of the single-atom profile with the
     Maxwell-Boltzmann velocity distribution yields the following expression for the
@@ -55,11 +55,58 @@ class Random;
     \,\frac{c}{v_\mathrm{th}} \f] with \f$A_\alpha\f$ the Einstein A-coefficient of the Lya
     transition; and the Voigt function \f$H(a_\mathrm{v},x)\f$ is defined by \f[ H(a_\mathrm{v},x)
     = \frac{a_\mathrm{v}}{\pi} \int_{-\infty}^\infty \frac{\mathrm{e}^{-y^2} \,\mathrm{d}y}
-    {(y-x)^2+a_\mathrm{v}^2} \f] which is normalized so that \f$H(a_\mathrm{v},0) = 1\f$. */
+    {(y-x)^2+a_\mathrm{v}^2} \f] which is normalized so that \f$H(a_\mathrm{v},0) = 1\f$.
+
+    <b>Frequency shift due to atom velocity</b>
+
+    In most astrophysical conditions, the energy of the Lyman-alpha photon before and after
+    scattering is identical in the frame of the interacting atom. This is because the life time of
+    the atom in its 2p state is very short so that it is not perturbed over this short time
+    interval. Because of the random thermal motion of the atom, energy conservation in the atom's
+    frame translates to a change in the energy of the incoming and outgoing photon that depends on
+    the velocity of the atom and the scattering direction. Given the velocity of the atom
+    \f$\bf{v}\f$, we define the dimensionless atom velocity as
+    \f${\bf{u}}={\bf{v}}/v_\mathrm{th}\f$. Denoting the propagation direction and dimensionless
+    frequency of the photon before (after) scattering with \f$\bf{k}_\mathrm{in}\f$ and
+    \f$x_\mathrm{in}\f$ (\f$\bf{k}_\mathrm{out}\f$ and \f$x_\mathrm{out}\f$), the resulting
+    frequency change can be written as \f[x_\mathrm{out} = x_\mathrm{in} -
+    {\bf{u}}\cdot{\bf{k}}_\mathrm{in} + {\bf{u}}\cdot{\bf{k}}_\mathrm{out} \f] This analysis
+    ignores the energy transferred from the photon to the atom through recoil, an approximation
+    that is justified in regular astrophysical conditions.
+
+    Assuming a Maxwell-Boltzmann velocity distribution for the atoms, the two components of the
+    dimensionless atom velocity \f$\bf{u}\f$ that are orthogonal to the incoming photon direction
+    \f$\bf{k}_\mathrm{in}\f$ have a Gaussian probability distribution with zero mean and a standard
+    deviation of \f$1/\sqrt{2}\f$. The parallel component is more complicated. We denote the
+    dimensionless atom velocity component parallel to the incoming photon direction as
+    \f$u_\parallel\f$. The probability distribution \f$P(u_\parallel|x_\mathrm{in})\f$ for this
+    component is proportional to both the Gaussian atom velocity distribution and the Lyman-alpha
+    scattering cross section for a single atom, reflecting the preference for photons to be
+    scattered by atoms to which they appear close to resonance. With the proper normalization this
+    leads to \f[ P(u_\parallel|x_\mathrm{in}) = \frac{a_\mathrm{v}}{\pi
+    H(a_\mathrm{v},x_\mathrm{in})}\,
+    \frac{\mathrm{e}^{-u_\parallel^2}}{(u_\parallel-x_\mathrm{in})^2+a_\mathrm{v}^2} \f]
+
+    <b>Scattering phase function</b>
+
+    Lyman-alpha scattering takes one of two forms: isotropic scattering or dipole scattering; the
+    latter is also called Rayleigh scattering. The corresponding phase functions depend only on the
+    cosine of the scattering angle \f$\mu={\bf{k}}_\mathrm{in}\cdot{\bf{k}}_\mathrm{out}\f$. With
+    normalization \f$\int_{-1}^1 P(\mu)\,\mathrm{d}\mu = 2\f$ these phase functions can be written,
+    respectively, as \f[ P(\mu) = 1 \f] and \f[ P(\mu) = \frac{3}{4}(\mu^2+1) \f]
+
+    Quantum mechanical considerations lead to a simple recipe for selecting the appropriate phase
+    function depending on whether the incoming photon frequency is in the core or in the wings of
+    the cross section. The recipe prescribes to treat 1/3 of all core scattering events as dipole,
+    and the remaining 2/3 as isotropic; and to treat all wing scattering events as dipole. For the
+    purpose of this recipe, the scattering event is considered to occur in the core if the incoming
+    dimensionless photon frequency in the rest frame of the interacting atom is smaller than a
+    critical value, \f$|x|<0.2\f$. */
 namespace LyaUtils
 {
     /** This function returns the Lyman-alpha scattering cross section per hydrogen atom
-        \f$\sigma_\alpha(\lambda, T)\f$ at the given photon wavelength and gas temperature. */
+        \f$\sigma_\alpha(\lambda, T)\f$ at the given photon wavelength and gas temperature, using
+        the definition given in the class header. */
     double section(double lambda, double T);
 
     /** This function draws a random hydrogen atom velocity as seen by an incoming photon from the
@@ -67,10 +114,34 @@ namespace LyaUtils
         scattered by atoms to which they appear close to resonance. In addition, it determines
         whether the photon scatters through the isotropic or dipole phase function.
 
-        The first item in the returned pair is the atom velocity; the second item is true for the
-        dipole phase function and false for isotropic scattering.
+        The function arguments include the photon packet wavelength as it is perceived in the local
+        gas frame, the gas temperature, the hydrogen number density, and the length of the photon
+        packet path in the current spatial cell. The latter two values are used in some of the
+        acceleration schemes discussed below. The return value is a pair: the first item is the
+        atom velocity and the second item is true for the dipole phase function and false for
+        isotropic scattering.
 
-        TODO: describe all of this in much more detail. */
+        The function proceeds as follows:
+
+        - Convert the perceived photon packet wavelength to the corresponding dimensionless
+        frequency using its definition given in the class header.
+
+        - Draw values for the components of the dimensionless atom velocity parallel and orthogonal
+        to the incoming photon packet from the probability distribution described in the class
+        header and from Gaussian distributions, respectively.
+
+        - Transform the dimensionless frequency into the rest frame of the atom as described in the
+        class header.
+
+        - Select the isotropic or the dipole phase functions according to the scheme discussed in
+        the class header, using the dimensionless frequency in the atom frame.
+
+        - Multiply the dimensionless atom velocity by the thermal velocity to obtain the actual
+        physical atom velocity.
+
+        - Return the atom velocity and a flag indicating the selected phase function.
+
+        */
     std::pair<Vec, bool> sampleAtomVelocity(double lambda, double T, double nH, double ds, Direction kin,
                                             Configuration* config, Random* random);
 
@@ -101,16 +172,18 @@ namespace LyaUtils
         maximum column density \f$\mathcal{N}^*\f$ through the simulation domain, which can be
         estimated from the input model through the maximum hydrogen number density and the maximum
         path length, we can write this requirement using the definition of the Lyman-alpha cross
-        section as \f[ \tau^* = \mathcal{N}^* \,\sigma_{\alpha,0}\,H(a_\mathrm{v},x^*) \f] where
-        \f$x^*\f$ is the dimensionless frequency shift to be calculated. Assuming that the required
-        maximum optical depth \f$\tau^*\f$ is sufficiently low for \f$x^*\f$ to be in the wings of
-        the Voigt profile, we can use the corresponding approximation of the Voigt profile. After
-        substituting the definitions of the other quantities, we obtain \f[ v_\mathrm{p,Voigt}^* =
-        \sqrt{\frac{3 A_\alpha^2 \lambda_\alpha^4}{64 \pi^3} \, \frac{\mathcal{N}^*}{\tau^*}}. \f]
-        Interestingly, the dependence on temperature (hidden in \f$\sigma_{\alpha,0}\f$,
-        \f$a_\mathrm{v}\f$ and \f$x^*\f$) has canceled out of this equation.
+        section given in the class header as \f[ \tau^* = \mathcal{N}^* \,\sigma_{\alpha,0}
+        \,H(a_\mathrm{v},x^*) \f] where \f$x^*\f$ is the dimensionless frequency shift to be
+        calculated. Assuming that the required maximum optical depth \f$\tau^*\f$ is sufficiently
+        low for \f$x^*\f$ to be in the wings of the Voigt profile, we can use the corresponding
+        approximation \f$ H(a_\mathrm{v},x^*) \approx a_\mathrm{v} / \sqrt{\pi}x^{*2} \f$. After
+        substituting the definitions of the other quantities as given in the class header, we
+        obtain \f[ v_\mathrm{p,Voigt}^* = \sqrt{\frac{3 A_\alpha^2 \lambda_\alpha^4}{64 \pi^3} \,
+        \frac{\mathcal{N}^*}{\tau^*}}. \f] Interestingly, the dependence on temperature (hidden in
+        \f$\sigma_{\alpha,0}\f$, \f$a_\mathrm{v}\f$ and \f$x^*\f$) has canceled out of this
+        equation.
 
-        The current implemenation uses \f$\tau^*=10^{-3}\f$.
+        The current implementation uses \f$\tau^*=10^{-3}\f$.
 
         The function arguments are: the maximum source velocity magnitude, the maximum medium bulk
         velocity magnitude, the maximum hydrogen number density, and the maximum photon packet path
