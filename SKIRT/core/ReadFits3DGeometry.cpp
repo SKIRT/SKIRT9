db97a19e7c918f0d5fbbb22644b6317efd689b8f
Peter Camps <peter.camps@ugent.be>
2020-11-06 10:08:11 +0100

    minor changes; mostly formatting

--- SKIRT/core/ReadFits3DGeometry.cpp
+++ SKIRT/core/ReadFits3DGeometry.cpp
@@ -4,7 +4,6 @@
 ///////////////////////////////////////////////////////////////// */
 
 #include "ReadFits3DGeometry.hpp"
-#include "FatalError.hpp"
 #include "FITSInOut.hpp"
 #include "NR.hpp"
 #include "Random.hpp"
@@ -25,53 +24,48 @@ void ReadFits3DGeometry::setupSelfBefore()
     NR::cdf(_Xv, _datacube);
 
     // Calculate the boundaries of the datacube in physical coordinates
-    _xmin = -(_nx/2.)*_pixelScale;
-    _xmax =  (_nx/2.)*_pixelScale;
-    _ymin = -(_ny/2.)*_pixelScale;
-    _ymax =  (_ny/2.)*_pixelScale;
-    _zmin = -(_nz/2.)*_pixelScale;
-    _zmax =  (_nz/2.)*_pixelScale;
-
+    _xmin = -(_nx / 2.) * _pixelScale;
+    _xmax = (_nx / 2.) * _pixelScale;
+    _ymin = -(_ny / 2.) * _pixelScale;
+    _ymax = (_ny / 2.) * _pixelScale;
+    _zmin = -(_nz / 2.) * _pixelScale;
+    _zmax = (_nz / 2.) * _pixelScale;
 }
 
 ////////////////////////////////////////////////////////////////////
 
 double ReadFits3DGeometry::density(Position bfr) const
 {
-    double x,y,z;
-    bfr.cartesian(x,y,z);
+    double x, y, z;
+    bfr.cartesian(x, y, z);
 
     // Find the corresponding spaxel in the datacube
-    int i = static_cast<int>(floor((x-_xmin)/_pixelScale));
-    int j = static_cast<int>(floor((y-_ymin)/_pixelScale));
-    int k = static_cast<int>(floor((z-_zmin)/_pixelScale));
-    if (i<0 || i>=_nx || j<0 || j>=_ny|| k<0 || k>=_nz) return 0.0;
+    int i = static_cast<int>(floor((x - _xmin) / _pixelScale));
+    int j = static_cast<int>(floor((y - _ymin) / _pixelScale));
+    int k = static_cast<int>(floor((z - _zmin) / _pixelScale));
+    if (i < 0 || i >= _nx || j < 0 || j >= _ny || k < 0 || k >= _nz) return 0.;
 
     // Return the density
-    return _datacube[i + _nx*j + _nx*_ny*k];
+    return _datacube[i + _nx * j + _nx * _ny * k];
 }
 
 ////////////////////////////////////////////////////////////////////
 
 Position ReadFits3DGeometry::generatePosition() const
 {
-    // Draw a random position in cube based on the
-    // cumulative distribution in each spaxel
-    double X1 = random()->uniform();
-    int Xindex = NR::locate(_Xv,X1);
-
-    int i = (Xindex%(_nx*_ny))%_nx;
-    int j = ((Xindex-i)%(_nx*_ny))/_nx;
-    int k = (Xindex-_nx*j - i)/(_nx*_ny);
+    // Draw a random position in cube based on the cumulative density distribution
+    int index = NR::locate(_Xv, random()->uniform());
+    int i = (index % (_nx * _ny)) % _nx;
+    int j = ((index - i) % (_nx * _ny)) / _nx;
+    int k = (index - _nx * j - i) / (_nx * _ny);
 
     // Determine the x, y and z coordinate in the datacube
-    double x = _xmin + (i+random()->uniform())*_pixelScale;
-    double y = _ymin + (j+random()->uniform())*_pixelScale;
-    double z = _zmin + (k+random()->uniform())*_pixelScale;
-
+    double x = _xmin + (i + random()->uniform()) * _pixelScale;
+    double y = _ymin + (j + random()->uniform()) * _pixelScale;
+    double z = _zmin + (k + random()->uniform()) * _pixelScale;
 
     // Return the position
-    return Position(x,y,z);
+    return Position(x, y, z);
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -80,16 +74,16 @@ double ReadFits3DGeometry::SigmaX() const
 {
     double sum = 0;
     const int NSAMPLES = 10000;
-    double step = (_xmax-_xmin)/NSAMPLES;
+    double step = (_xmax - _xmin) / NSAMPLES;
 
     // For each position, get the density and add it to the total
     for (int k = 0; k < NSAMPLES; k++)
     {
-        sum += density(Position(_xmin + k*step, 0, 0));
+        sum += density(Position(_xmin + k * step, 0, 0));
     }
 
     // Return the x-axis surface density
-    return sum*step;
+    return sum * step;
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -98,16 +92,16 @@ double ReadFits3DGeometry::SigmaY() const
 {
     double sum = 0;
     const int NSAMPLES = 10000;
-    double step = (_ymax-_ymin)/NSAMPLES;
+    double step = (_ymax - _ymin) / NSAMPLES;
 
     // For each position, get the density and add it to the total
     for (int k = 0; k < NSAMPLES; k++)
     {
-        sum += density(Position(_ymin + k*step, 0, 0));
+        sum += density(Position(_ymin + k * step, 0, 0));
     }
 
     // Return the y-axis surface density
-    return sum*step;
+    return sum * step;
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -116,16 +110,16 @@ double ReadFits3DGeometry::SigmaZ() const
 {
     double sum = 0;
     const int NSAMPLES = 10000;
-    double step = (_zmax-_zmin)/NSAMPLES;
+    double step = (_zmax - _zmin) / NSAMPLES;
 
     // For each position, get the density and add it to the total
     for (int k = 0; k < NSAMPLES; k++)
     {
-        sum += density(Position(0, _zmin + k*step, 0));
+        sum += density(Position(0, _zmin + k * step, 0));
     }
 
     // Return the z-axis surface density
-    return sum*step;
+    return sum * step;
 }
 
 ////////////////////////////////////////////////////////////////////
--- SKIRT/core/ReadFits3DGeometry.hpp
+++ SKIRT/core/ReadFits3DGeometry.hpp
@@ -13,15 +13,15 @@
 
 /** The ReadFits3DGeometry class is a subclass of the GenGeometry class, and describes an arbitary
     3D geometry for a single component, read from a 3D (.fits file) datacube. The model geometry is
-    set by two parameters: the input filename and the pixelscale \f$pix\f$. */
-class ReadFits3DGeometry: public GenGeometry
+    set by two parameters: the input filename and the pixel scale. */
+class ReadFits3DGeometry : public GenGeometry
 {
     ITEM_CONCRETE(ReadFits3DGeometry, GenGeometry, "a 3D geometry read from a FITS file")
-        //ATTRIBUTE_TYPE_DISPLAYED_IF(ReadFits3DGeometry, "Level2")
+        ATTRIBUTE_TYPE_DISPLAYED_IF(ReadFits3DGeometry, "Level2")
 
-    PROPERTY_STRING(filename, "the filename of the datacube")
+        PROPERTY_STRING(filename, "the filename of the datacube")
 
-    PROPERTY_DOUBLE(pixelScale, "the pixelscale for the datacube (i.e. the physical length per pixel)")
+        PROPERTY_DOUBLE(pixelScale, "the pixel scale for the datacube (i.e. the physical length per pixel)")
         ATTRIBUTE_QUANTITY(pixelScale, "length")
         ATTRIBUTE_MIN_VALUE(pixelScale, "]0")
 
@@ -30,8 +30,9 @@ class ReadFits3DGeometry: public GenGeometry
     //============= Construction - Setup - Destruction =============
 
 protected:
-    /** This function reads in the .fits datacube. A vector with the normalized cumulative distribution in
-        each spaxel is computed, satisfying the condition that the total mass equals 1. */
+    /** This function reads in the .fits datacube. A vector with the normalized cumulative
+        distribution in each spaxel is computed, satisfying the condition that the total mass
+        equals 1. */
     void setupSelfBefore() override;
 
     //======================== Other Functions =======================
@@ -40,17 +41,18 @@ public:
     /** This function returns the density \f$\rho(x,y,z)\f$ at the position (x,y,z). */
     double density(Position bfr) const override;
 
-    /** This function generates a random position \f$(x,y,z)\f$ from the geometry, by drawing a random point
-        from the appropriate probability density distribution function. The \f$(x,y,z)\f$ coordinates are
-        derived from the normalized cumulative distribution vector. */
+    /** This function generates a random position \f$(x,y,z)\f$ from the geometry, by drawing a
+        random point from the appropriate probability density distribution function. The
+        \f$(x,y,z)\f$ coordinates are derived from the normalized cumulative distribution vector.
+        */
     Position generatePosition() const override;
 
-    /** This function returns the X-axis surface density, i.e. the integration of the density along the
-        entire X-axis, \f[ \Sigma_X = \int_{-\infty}^\infty \rho(x,0,0) \, \text{d} x \f] */
+    /** This function returns the X-axis surface density, i.e. the integration of the density along
+        the entire X-axis, \f[ \Sigma_X = \int_{-\infty}^\infty \rho(x,0,0) \, \text{d} x \f] */
     double SigmaX() const override;
 
-    /** This function returns the Y-axis surface density, i.e. the integration of the density along the
-        entire Y-axis, \f[ \Sigma_Y = \int_{-\infty}^\infty \rho(0,y,0) \, \text{d} y \f] */
+    /** This function returns the Y-axis surface density, i.e. the integration of the density along
+        the entire Y-axis, \f[ \Sigma_Y = \int_{-\infty}^\infty \rho(0,y,0) \, \text{d} y \f] */
     double SigmaY() const override;
 
     /** This function returns the Z-axis surface density, i.e. the integration of the density along
@@ -60,15 +62,13 @@ public:
     //======================== Data Members ========================
 
 private:
-
     // the input datacube and the cumulative distribution
     Array _datacube;
     int _nx{0}, _ny{0}, _nz{0};
     Array _Xv;
 
     // other data members initialized during setup
-    double _xmax{0.}, _ymax{0.}, _zmax{0.}, _xmin{0.}, _ymin{0.}, _zmin{0.};
-
+    double _xmin{0.}, _xmax{0.}, _ymin{0.}, _ymax{0.}, _zmin{0.}, _zmax{0.};
 };
 
 ////////////////////////////////////////////////////////////////////
