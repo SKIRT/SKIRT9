53dd357e4e9d839f6ef5669fba4b0cb9fa6d56e6
Peter Camps <peter.camps@ugent.be>
2021-11-19 15:01:49 +0100

    add Compton cross section and wavelength shift

--- SKIRT/core/ComptonPhaseFunction.cpp
+++ SKIRT/core/ComptonPhaseFunction.cpp
@@ -4,7 +4,22 @@
 ///////////////////////////////////////////////////////////////// */
 
 #include "ComptonPhaseFunction.hpp"
+#include "Constants.hpp"
 #include "Random.hpp"
+
+////////////////////////////////////////////////////////////////////
+
+namespace
+{
+    // returns the photon energy scaled to the electron rest energy: h nu / m_e c^2
+    double scaledEnergy(double lambda) { return (Constants::h() / Constants::Melectron() / Constants::c()) / lambda; }
+
+    // returns the inverse Compton factor for a given scaled energy and scattering angle cosine
+    double inverseComptonfactor(double x, double costheta) { return 1 + x * (1 - costheta); }
+
+    // returns the Compton factor for a given scaled energy and scattering angle cosine
+    ///double straightComptonfactor(double x, double costheta) { return 1. / inverseComptonfactor(x, costheta); }
+}
 
 ////////////////////////////////////////////////////////////////////
 
@@ -16,21 +31,26 @@ void ComptonPhaseFunction::initialize(Random* random)
 
 ////////////////////////////////////////////////////////////////////
 
-double ComptonPhaseFunction::sectionSca(double /*lambda*/) const
+double ComptonPhaseFunction::sectionSca(double lambda) const
 {
-    return 1.;
+    double x = scaledEnergy(lambda);
+    double x2 = x * x;
+    double x3 = x2 * x;
+    double x1 = 1. + x;
+    double x12 = 1. + 2. * x;
+    return 0.75 * (x1 / (x12 * x12) + 2. / x2 + (1. / (2. * x) - x1 / x3) * log(x12));
 }
 
 ////////////////////////////////////////////////////////////////////
 
-double ComptonPhaseFunction::phaseFunctionValueForCosine(double costheta) const
+double ComptonPhaseFunction::phaseFunctionValueForCosine(double /*x*/, double costheta) const
 {
     return 0.75 * (costheta * costheta + 1.);
 }
 
 ////////////////////////////////////////////////////////////////////
 
-double ComptonPhaseFunction::generateCosineFromPhaseFunction() const
+double ComptonPhaseFunction::generateCosineFromPhaseFunction(double /*x*/) const
 {
     double X = _random->uniform();
     double p = cbrt(4. * X - 2. + sqrt(16. * X * (X - 1.) + 5.));
@@ -39,23 +59,32 @@ double ComptonPhaseFunction::generateCosineFromPhaseFunction() const
 
 ////////////////////////////////////////////////////////////////////
 
-void ComptonPhaseFunction::peeloffScattering(double& I, double& /*lambda*/, double w, Direction bfk,
-                                             Direction bfkobs) const
+void ComptonPhaseFunction::peeloffScattering(double& I, double& lambda, double w, Direction bfk, Direction bfkobs) const
 {
-    // calculate the value of the material-specific phase function
+    double x = scaledEnergy(lambda);
+
+    // calculate the value of the phase function
     double costheta = Vec::dot(bfk, bfkobs);
-    double value = phaseFunctionValueForCosine(costheta);
+    double value = phaseFunctionValueForCosine(x, costheta);
 
     // accumulate the weighted sum in the intensity
     I += w * value;
+
+    // adjust the wavelength
+    lambda *= inverseComptonfactor(x, costheta);
 }
 
 ////////////////////////////////////////////////////////////////////
 
-Direction ComptonPhaseFunction::performScattering(double& /*lambda*/, Direction bfk) const
+Direction ComptonPhaseFunction::performScattering(double& lambda, Direction bfk) const
 {
-    // sample a scattering angle from the dipole phase function
-    double costheta = generateCosineFromPhaseFunction();
+    double x = scaledEnergy(lambda);
+
+    // sample a scattering angle from the phase function
+    double costheta = generateCosineFromPhaseFunction(x);
+
+    // adjust the wavelength
+    lambda *= inverseComptonfactor(x, costheta);
 
     // determine the new propagation direction
     return _random->direction(bfk, costheta);
--- SKIRT/core/ComptonPhaseFunction.hpp
+++ SKIRT/core/ComptonPhaseFunction.hpp
@@ -12,8 +12,26 @@ class Random;
 
 ////////////////////////////////////////////////////////////////////
 
-/** The ComptonPhaseFunction helper class represents the Compton scattering process. The current
-    implementation does not support polarization. */
+/** The ComptonPhaseFunction helper class represents Compton scattering of photons by free
+    electrons. The current implementation does not support polarization.
+
+    Compton scattering forms the extension of Thomson scattering into the high-photon-energy
+    domain. The scattering event is elastic (i.e. the photon wavelength is changed by the
+    interaction) and its cross section and phase function depend on the wavelength of the incoming
+    photon. Compared to Thomson scattering, the total cross section is reduced at higher energies.
+    With \f$x = (h\nu)/(m_e c^2)\f$ the incoming photon energy scaled to the electron rest energy,
+    the ratio of the total Compton scattering cross section \f$\sigma_\mathrm{C}(x)\f$ over the
+    (constant) total Thomson cross section \f$\sigma_\mathrm{T}\f$ is given by:
+
+    \f[ \frac{\sigma_\mathrm{C}(x)}{\sigma_\mathrm{T}} = \frac{3}{4} \bigg( \frac{1+x}{(1+2x)^2} +
+    \frac{2}{x^2} + \Big(\frac{1}{2x}-\frac{1+x}{x^3}\Big) \cdot \ln(1+2x) \bigg). \f]
+
+    For an interaction with incoming photon energy \f$x\f$ and scattering angle \f$\theta\f$, the
+    energy change \f$E_\mathrm{out}/E_\mathrm{in}\f$ is given by the Compton factor \f$C(x, \theta)
+    = \big(1+x(1-\cos \theta)\big)^{-1}\f$. Equivalently, the wavelength change is given by
+    \f$\lambda_\mathrm{out} / \lambda_\mathrm{in} = \big(1+x(1-\cos \theta)\big)\f$.
+
+    */
 class ComptonPhaseFunction
 {
     //============= Construction - Setup - Destruction =============
@@ -33,14 +51,15 @@ public:
     //======== Scattering =======
 
 private:
-    /** This function returns the value of the scattering phase function \f$\Phi(\cos\theta)\f$ for
-        the specified scattering angle cosine \f$\cos\theta\f$, where the phase function is
-        normalized as \f[\int_{-1}^1 \Phi(\cos\theta) \,\mathrm{d}\cos\theta =2.\f] */
-    double phaseFunctionValueForCosine(double costheta) const;
+    /** This function returns the value of the scattering phase function \f$\Phi(x, \cos\theta)\f$
+        for the specified incoming photon energy \f$x\f$ and scattering angle cosine
+        \f$\cos\theta\f$, where the phase function is normalized for all \f$x\f$ as \f[\int_{-1}^1
+        \Phi(x, \cos\theta) \,\mathrm{d}\cos\theta =2.\f] */
+    double phaseFunctionValueForCosine(double x, double costheta) const;
 
     /** This function generates a random scattering angle cosine sampled from the phase function
-        \f$\Phi(\cos\theta)\f$. */
-    double generateCosineFromPhaseFunction() const;
+        \f$\Phi(x, \cos\theta)\f$ for the specified incoming photon energy \f$x\f$. */
+    double generateCosineFromPhaseFunction(double x) const;
 
 public:
     /** This function calculates the contribution of a Compton scattering event to the peel-off
