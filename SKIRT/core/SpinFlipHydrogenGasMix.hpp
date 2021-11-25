ff4ab6946bdeadb8f5ffabcc60377a02bb4cfde3
Peter Camps <peter.camps@ugent.be>
2021-11-25 17:04:35 +0100

    increase maximum gas temperature

--- SKIRT/core/SpinFlipHydrogenGasMix.hpp
+++ SKIRT/core/SpinFlipHydrogenGasMix.hpp
@@ -33,7 +33,7 @@ class SpinFlipHydrogenGasMix : public MaterialMix
         PROPERTY_DOUBLE(defaultTemperature, "the default temperature of the gas")
         ATTRIBUTE_QUANTITY(defaultTemperature, "temperature")
         ATTRIBUTE_MIN_VALUE(defaultTemperature, "[3")  // gas temperature must be above local Universe T_CMB
-        ATTRIBUTE_MAX_VALUE(defaultTemperature, "1e6]")
+        ATTRIBUTE_MAX_VALUE(defaultTemperature, "1e9]")
         ATTRIBUTE_DEFAULT_VALUE(defaultTemperature, "1e4")
         ATTRIBUTE_DISPLAYED_IF(defaultTemperature, "Level2")
 
