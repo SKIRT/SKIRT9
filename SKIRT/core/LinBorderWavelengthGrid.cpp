b413a937daa1a7674e84a5a01c4401b289e9c90b
Peter Camps <peter.camps@ugent.be>
2021-12-09 14:07:10 +0100

    add first untested border WLG

---
+++ SKIRT/core/LinBorderWavelengthGrid.cpp
@@ -0,0 +1,25 @@
+/*//////////////////////////////////////////////////////////////////
+////     The SKIRT project -- advanced radiative transfer       ////
+////       © Astronomical Observatory, Ghent University         ////
+///////////////////////////////////////////////////////////////// */
+
+#include "LinBorderWavelengthGrid.hpp"
+#include "FatalError.hpp"
+#include "NR.hpp"
+
+////////////////////////////////////////////////////////////////////
+
+void LinBorderWavelengthGrid::setupSelfBefore()
+{
+    DisjointWavelengthGrid::setupSelfBefore();
+
+    // verify property values
+    if (_maxWavelength <= _minWavelength) throw FATALERROR("the longest wavelength should be larger than the shortest");
+
+    // construct the grid
+    Array borderv;
+    NR::buildLinearGrid(borderv, _minWavelength, _maxWavelength, _numWavelengthBins);
+    setWavelengthBorders(borderv, false);
+}
+
+////////////////////////////////////////////////////////////////////
