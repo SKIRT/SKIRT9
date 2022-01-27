997fb9ff1a9983f168f5148740be176dc4f2b3d4
Peter Camps <peter.camps@ugent.be>
2022-01-27 16:26:21 +0100

    add power-law scaling to radial vector field

--- SKIRT/core/RadialVectorField.hpp
+++ SKIRT/core/RadialVectorField.hpp
@@ -10,14 +10,32 @@
 
 //////////////////////////////////////////////////////////////////////
 
-/** RadialVectorField represents a vector field with vectors pointing away from the origin with
-    lengths that depend on the spatial location and on a scale radius \f$r_1\f$ specified by the
-    user as a property. Specifically, the magnitude \f$v\f$ of the vectors is given by \f[ v =
-    \begin{cases} r/r_1 & \mathrm{for}\;0\le r<r_1 \\ 1 & \mathrm{for}\;r\ge r_1 \end{cases} \f] or
-    by \f$v=1\f$ if \f$r_1=0\f$ (the default value). In other words, for the default transition
-    radius of zero, the field consists of unit vectors. For other transition radii, the vector
-    magnitude scales from zero at \f$r=0\f$ to unity at \f$r=r_1\f$, and stays at unity for
-    \f$r>r_1\f$. */
+/** RadialVectorField represents a vector field with vectors pointing away from the origin and with
+    a magnitude that varies with radial distance from the origin. The class has two configuration
+    properties: the distance at which the vector magnitude becomes unity and a power law exponent.
+    If the exponent is positive, the magnitude ramps up from zero at the origin to unity at the
+    specified distance, and remains unity from there on. If the power law index is negative, the
+    magnitude is unity from the origin to the specified distance, and then ramps down from there to
+    zero at infinity.
+
+    Given the user-configurable scale radius \f$r_1\ge 0\f$ and power-law exponent \f$\alpha\f$,
+    the magnitude \f$v(r)\f$ of the vectors as a function of radial distance \f$r\f$ is given by
+
+    \f[ r_1=0 \lor \alpha=0 \qquad v(r) = \begin{cases} 0 & \mathrm{for}\;r=0 \\ 1 &
+    \mathrm{for}\;r>0 \end{cases} \f]
+
+    \f[ r_1>0 \land \alpha>0 \qquad v(r) = \begin{cases} 0 & \mathrm{for}\;r=0 \\ (r/r_1)^\alpha &
+    \mathrm{for}\;0<r<r_1 \\ 1 & \mathrm{for}\;r\ge r_1 \end{cases} \f]
+
+    \f[ r_1>0 \land \alpha<0 \qquad v(r) = \begin{cases} 0 & \mathrm{for}\;r=0 \\ 1 &
+    \mathrm{for}\;0<r<r_1 \\ (r/r_1)^\alpha & \mathrm{for}\;r \ge r_1 \end{cases} \f]
+
+    The first equation defines the vector field for the special values \f$r_1=0\f$ and/or
+    \f$\alpha=0\f$ to consist solely of unit vectors except at the origin, where the direction of
+    the vector is undefined. The default value of \f$r_1=0\f$ causes this behavior. If the unity
+    radius is set to a value \f$r_1>0\f$, the default value of \f$\alpha=1\f$ specifies linear
+    behavior: the vector magnitude scales linearly from zero at the origin to unity at \f$r=r_1\f$,
+    and stays at unity for larger radii. */
 class RadialVectorField : public VectorField
 {
     ITEM_CONCRETE(RadialVectorField, VectorField, "a vector field pointing away from the origin")
@@ -27,6 +45,9 @@ class RadialVectorField : public VectorField
         ATTRIBUTE_QUANTITY(unityRadius, "length")
         ATTRIBUTE_MIN_VALUE(unityRadius, "[0")
         ATTRIBUTE_DEFAULT_VALUE(unityRadius, "0")
+
+        PROPERTY_DOUBLE(exponent, "the power-law exponent governing the radial dependence of the magnitude")
+        ATTRIBUTE_DEFAULT_VALUE(exponent, "1")
 
     ITEM_END()
 
