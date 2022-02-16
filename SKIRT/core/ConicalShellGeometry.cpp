9b73b9358620a14165a8fdfb3f89e36e81377c2d
Peter Camps <peter.camps@ugent.be>
2022-02-16 15:42:01 +0100

    Correct random generation of positions for the Shell and ConicalShell geometries (#134)

--- SKIRT/core/ConicalShellGeometry.cpp
+++ SKIRT/core/ConicalShellGeometry.cpp
@@ -24,6 +24,8 @@ void ConicalShellGeometry::setupSelfBefore()
     _cosDelta = cos((_DeltaOut + _DeltaIn) / 2.);
     _smin = SpecialFunctions::gln(_p - 2.0, _rmin);
     _sdiff = SpecialFunctions::gln2(_p - 2.0, _rmax, _rmin);
+    _tmin = pow(_rmin, 3.0 - _p);
+    _tmax = pow(_rmax, 3.0 - _p);
 
     // determine the normalization factor
     if (_q > 1e-3)
@@ -62,8 +64,18 @@ Position ConicalShellGeometry::generatePosition() const
     while (true)
     {
         double X = random()->uniform();
-        double s = _smin + X * _sdiff;
-        double r = SpecialFunctions::gexp(_p - 2.0, s);
+        double r = 0.0;
+        if (fabs(_p - 3.0) < 1e-2)
+        {
+            double s = _smin + X * _sdiff;
+            r = SpecialFunctions::gexp(_p - 2.0, s);
+        }
+        else
+        {
+            double z = (1.0 - X) * _tmin + X * _tmax;
+            r = pow(z, 1.0 / (3.0 - _p));
+        }
+
         X = random()->uniform();
         double costheta = 0.0;
         if (_q < 1e-3)
--- SKIRT/core/ConicalShellGeometry.hpp
+++ SKIRT/core/ConicalShellGeometry.hpp
@@ -16,13 +16,13 @@
     by a radial power-law density (see Stalevski et al. 2012, MNRAS, 420, 2756–2772) but with two
     finite opening angles, inner and outer. In formula, it is most easily expressed in spherical
     coordinates as \f[ \rho(r,\theta) = A\, r^{-p}\,{\text{e}}^{-q|\cos\theta|} \quad\text{for }
-    r_{\text{min}}<r<r_{\text{max}} \text{ and }
-    \frac{\pi}{2}-\Delta_{\text{out}}<\theta<\frac{\pi}{2}-\Delta_{\text{in}} \text{ and }
-    \frac{\pi}{2}+\Delta_{\text{in}}<\theta<\frac{\pi}{2}+\Delta_{\text{out}}. \f] There are six
-    free parameters describing this dust geometry: the inner and outer radii \f$r_{\text{min}}\f$
-    and \f$r_{\text{max}}\f$ of the conical shell, the radial power law index \f$p\f$, the polar
-    index \f$q\f$ and the inner and outer opening angles \f$\Delta_{\text{in}}\f$ and
-    \f$\Delta_{\text{out}}\f$ describing the inner and outer edge of the shell.
+    r_{\text{min}}<r<r_{\text{max}} \text{ and } \frac{\pi}{2}-\Delta_{\text{out}} <
+    \theta<\frac{\pi}{2}-\Delta_{\text{in}} \text{ and } \frac{\pi}{2}+\Delta_{\text{in}} <
+    \theta<\frac{\pi}{2}+\Delta_{\text{out}}. \f] There are six free parameters describing this
+    dust geometry: the inner and outer radii \f$r_{\text{min}}\f$ and \f$r_{\text{max}}\f$ of the
+    conical shell, the radial power law index \f$p\f$, the polar index \f$q\f$ and the inner and
+    outer opening angles \f$\Delta_{\text{in}}\f$ and \f$\Delta_{\text{out}}\f$ describing the
+    inner and outer edge of the shell.
 
     If the dusty system under consideration is in the vicinity of an AGN central engine or another
     source which is luminous enough to heat the dust up to sublimation temperature, the inner
@@ -89,14 +89,14 @@ class ConicalShellGeometry : public AxGeometry
 protected:
     /** This function verifies the validity of the geometry parameters. The normalization parameter
         \f$A\f$ is set by the normalization condition that total mass equals one, i.e. \f[ 1 =
-        2\pi\, A\, 2\int_{\pi/2+\Delta_{\text{in}}}^{\pi/2+\Delta_{\text{out}}} e^{-q|\cos\theta|}\sin\theta\,
-        {\text{d}}\theta \int_{r_{\text{min}}}^{r_{\text{max}}} r^{2-p}\, {\text{d}}r. \f] This
-        results in \f[ A = \frac{q}{4\pi\, ({\text{e}}^{-q\sin\Delta_{\text{in}}} -
-        {\text{e}}^{-q\sin\Delta_{\text{out}}})}\, \frac{1}{ {\text{gln}}_{p-2}\, r_{\text{max}} -
-        {\text{gln}}_{p-2}\, r_{\text{min}} }, \f] with \f${\text{gln}}_p\, x\f$ the generalized
-        logarithm defined in SpecialFunctions::gln. If \f$q=0\f$, this expression reduces to \f[ A
-        = \frac{1}{4\pi\,(\sin\Delta_{\text{out}} - \sin\Delta_{\text{in}})\,
-        ({\text{gln}}_{p-2}\, r_{\text{max}} - {\text{gln}}_{p-2}\,
+        2\pi\, A\, 2\int_{\pi/2+\Delta_{\text{in}}}^{\pi/2+\Delta_{\text{out}}}
+        e^{-q|\cos\theta|}\sin\theta\, {\text{d}}\theta \int_{r_{\text{min}}}^{r_{\text{max}}}
+        r^{2-p}\, {\text{d}}r. \f] This results in \f[ A = \frac{q}{4\pi\,
+        ({\text{e}}^{-q\sin\Delta_{\text{in}}} - {\text{e}}^{-q\sin\Delta_{\text{out}}})}\,
+        \frac{1}{ {\text{gln}}_{p-2}\, r_{\text{max}} - {\text{gln}}_{p-2}\, r_{\text{min}} }, \f]
+        with \f${\text{gln}}_p\, x\f$ the generalized logarithm defined in SpecialFunctions::gln.
+        If \f$q=0\f$, this expression reduces to \f[ A = \frac{1}{4\pi\,(\sin\Delta_{\text{out}} -
+        \sin\Delta_{\text{in}})\, ({\text{gln}}_{p-2}\, r_{\text{max}} - {\text{gln}}_{p-2}\,
         r_{\text{min}} )}. \f] */
     void setupSelfBefore() override;
 
@@ -107,50 +107,52 @@ public:
         height \f$z\f$. It just implements the analytical formula. */
     double density(double R, double z) const override;
 
-    /** This function generates a random position from the torus geometry, by
-        drawing a random point from the three-dimensional probability density \f$p({\bf{r}})\,
-        {\text{d}}{\bf{r}} = \rho({\bf{r}})\, {\text{d}}{\bf{r}}\f$. For the torus geometry,
-        the density is a separable function of \f$r\f$ and \f$\theta\f$, so that a random
-        position can hence be constructed by combining random spherical coordinates, each chosen
-        from their own probability distributions. A random azimuth \f$\phi\f$ is readily found by
-        chosing a random deviate \f${\cal{X}}\f$ and setting \f$ \phi = 2\pi {\cal{X}} \f$.
+    /** This function generates a random position from the torus geometry, by drawing a random
+        point from the three-dimensional probability density \f$p({\bf{r}})\, {\text{d}}{\bf{r}} =
+        \rho({\bf{r}})\, {\text{d}}{\bf{r}}\f$. For the torus geometry, the density is a separable
+        function of \f$r\f$ and \f$\theta\f$, so that a random position can hence be constructed by
+        combining random spherical coordinates, each chosen from their own probability
+        distributions. A random azimuth \f$\phi\f$ is readily found by chosing a random deviate
+        \f${\cal{X}}\f$ and setting \f$ \phi = 2\pi {\cal{X}} \f$.
 
-        For the
-        radial coordinate, the appropriate probability distribution is \f$ p(r)\,{\text{d}}r \propto
-        r^{p-2}\,{\text{d}}r \f$. A random radius is generated by picking a new uniform deviate
-        \f${\cal{X}}\f$, and solving the equation \f[ {\cal{X}} = \int_0^r p(r')\, {\text{d}}r'
-        \f] for \f$r\f$. We obtain \f[ {\cal{X}} =
-        \frac{ {\text{gln}}_{p-2}\, r - {\text{gln}}_{p-2}\, r_{\text{min}} }{
-        {\text{gln}}_{p-2}\, r_{\text{max}} - {\text{gln}}_{p-2}\,
-        r_{\text{min}} }. \f] Inverting this equation results in \f[ r =
-        {\text{gexp}}_{p-2} \Big[ {\text{gln}}_{p-2}\, r_{\text{min}} + X (
-        {\text{gln}}_{p-2}\, r_{\text{max}} - {\text{gln}}_{p-2}\, r_{\text{min}} )
-        \Bigr]. \f] In these expressions, \f${\text{gln}}_p\,x\f$ and \f${\text{gexp}}_p\,x\f$ are the
-        generalized logarithm and exponential functions defined in
-        SpecialFunctions::gln and SpecialFunctions::gexp respectively.
+        For the radial coordinate, the appropriate probability distribution is \f$
+        p(r)\,{\text{d}}r \propto r^{2-p}\,{\text{d}}r \f$. A random radius is generated by picking
+        a new uniform deviate \f${\cal{X}}\f$, and solving the equation \f[ {\cal{X}} =
+        \int_{r_\text{min}}^r p(r')\, {\text{d}}r' \f] for \f$r\f$. For \f$p\ne3\f$ we find \f[
+        {\cal{X}} = \frac{r^{3-p} - r_{\text{min}}^{3-p}} {r_{\text{max}}^{3-p} -
+        r_{\text{min}}^{3-p}}. \f] Inverting this results in \f[ r = \left[
+        (1-{\cal{X}})\,r_{\text{min}}^{3-p} + {\cal{X}}\,r_{\text{max}}^{3-p}
+        \right]^{\frac{1}{3-p}}. \f] For \f$p=3\f$ this expression does not hold, and for
+        \f$p\approx3\f$ it breaks down numerically. So for \f$p\approx3\f$ we can write the general
+        expression \f[ r = {\text{gexp}}_{p-2} \Big[ {\text{gln}}_{p-2}\, r_{\text{min}} +
+        {\cal{X}}\,( {\text{gln}}_{p-2}\, r_{\text{max}} - {\text{gln}}_{p-2}\, r_{\text{min}} )
+        \Bigr]. \f] In this expression, \f${\text{gln}}_p\,x\f$ and \f${\text{gexp}}_p\,x\f$ are
+        the generalized logarithm and exponential functions defined in SpecialFunctions::gln and
+        SpecialFunctions::gexp respectively.
 
         Finally, for the polar angle, the appropriate distribution function is \f[ p(\theta)\,
-        {\text{d}}\theta \propto e^{-q|\cos\theta|}\sin\theta\, {\text{d}}\theta. \f] A random polar
-        angle is generated by picking a new uniform deviate \f${\cal{X}}\f$, and solving the equation
-        \f[ {\cal{X}} = \int_0^\theta p(\theta')\, {\text{d}}\theta' \f] for \f$\theta\f$. We obtain
-        after some calculation \f[ {\cal{X}} = \begin{cases} \; \dfrac12 \left( 1 -
+        {\text{d}}\theta \propto e^{-q|\cos\theta|}\sin\theta\, {\text{d}}\theta. \f] A random
+        polar angle is generated by picking a new uniform deviate \f${\cal{X}}\f$, and solving the
+        equation \f[ {\cal{X}} = \int_0^\theta p(\theta')\, {\text{d}}\theta' \f] for \f$\theta\f$.
+        We obtain after some calculation \f[ {\cal{X}} = \begin{cases} \; \dfrac12 \left( 1 -
         \dfrac{1-{\text{e}}^{-q\cos\theta}}{1-{\text{e}}^{-q\sin\Delta}} \right) & \quad\text{for }
-        \frac{\pi}{2}-\Delta < \theta < \frac{\pi}{2} \\[1.2em]  \;\dfrac12 \left( 1 +
+        \frac{\pi}{2}-\Delta < \theta < \frac{\pi}{2} \\[1.2em] \;\dfrac12 \left( 1 +
         \dfrac{1-{\text{e}}^{q\cos\theta}}{1-{\text{e}}^{-q\sin\Delta}} \right) & \quad\text{for }
-        \frac{\pi}{2} < \theta < \frac{\pi}{2}+\Delta \end{cases} \f] Inverting this
-        gives \f[ \cos\theta = \begin{cases}\; -\dfrac{1}{q} \ln\left[ 1-\left(1-
-        {\text{e}}^{-q\sin\Delta}\right) (1-2{\cal{X}}) \right] &
-        \quad\text{if $0<{\cal{X}}<\tfrac12$} \\[1.2em] \; \dfrac{1}{q} \ln\left[ 1-\left(1
-        -{\text{e}}^{-q\sin\Delta}\right) (2{\cal{X}}-1) \right] & \quad\text{if $\tfrac12<{\cal{X}}<1$}
-        \end{cases}. \f] Since this function generates a random position from the torus geometry, positions
-        are rejected until they fall into non-zere area, i.e. inside the conical shell. */
+        \frac{\pi}{2} < \theta < \frac{\pi}{2}+\Delta \end{cases} \f] Inverting this gives \f[
+        \cos\theta = \begin{cases}\; -\dfrac{1}{q} \ln\left[ 1-\left(1-
+        {\text{e}}^{-q\sin\Delta}\right) (1-2{\cal{X}}) \right] & \quad\text{if
+        $0<{\cal{X}}<\tfrac12$} \\[1.2em] \; \dfrac{1}{q} \ln\left[ 1-\left(1
+        -{\text{e}}^{-q\sin\Delta}\right) (2{\cal{X}}-1) \right] & \quad\text{if
+        $\tfrac12<{\cal{X}}<1$} \end{cases}. \f] Since this function generates a random position
+        from the torus geometry, positions are rejected until they fall into non-zere area, i.e.
+        inside the conical shell. */
     Position generatePosition() const override;
 
     /** This function returns the radial surface density, i.e. the integration of the density along
         a line going through the conical shell, in the plane half way between inner and outer edge
         of the shell, starting at the centre of the coordinate system, \f[ \Sigma_R = \int_0^\infty
-        \rho(r,\Delta)\,{\text{d}}r \quad\text{with}\quad \Delta=\frac{\Delta_{\text{in}}+\Delta_{\text{out}}}{2}. \f]
-        For the conical shell geometry, \f[ \Sigma_R = A\,
+        \rho(r,\Delta)\,{\text{d}}r \quad\text{with}\quad \Delta=\frac{\Delta_{\text{in}} +
+        \Delta_{\text{out}}}{2}. \f] For the conical shell geometry, \f[ \Sigma_R = A\,
         {\text{e}}^{-q\cos\Delta}\, ( {\text{gln}}_p\, r_{\text{max}} - {\text{gln}}_p\,
         r_{\text{min}} ) \f] with \f${\text{gln}}_p\,x\f$ the generalized logarithm defined in
         SpecialFunctions::gln. */
@@ -181,6 +183,8 @@ private:
     double _cosDelta{0.};
     double _smin{0.};
     double _sdiff{0.};
+    double _tmin{0.};
+    double _tmax{0.};
     double _A{0.};
 };
 
