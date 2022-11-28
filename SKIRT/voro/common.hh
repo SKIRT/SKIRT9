6b3a767879a4784ae3fec1b5ff19c7f8a5be425d
Peter Camps <peter.camps@ugent.be>
2022-11-28 15:12:11 +0100

    remove draw/output/print functions from voro

--- SKIRT/voro/common.hh
+++ SKIRT/voro/common.hh
@@ -21,11 +21,7 @@ namespace voro {
 void check_duplicate(int n,double x,double y,double z,int id,double *qp);
 
 void voro_fatal_error(const char *p,int status);
-void voro_print_positions(std::vector<double> &v,FILE *fp=stdout);
 FILE* safe_fopen(const char *filename,const char *mode);
-void voro_print_vector(std::vector<int> &v,FILE *fp=stdout);
-void voro_print_vector(std::vector<double> &v,FILE *fp=stdout);
-void voro_print_face_vertices(std::vector<int> &v,FILE *fp=stdout);
 
 }
 
