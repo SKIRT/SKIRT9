d2fc934c466ae659ce786af4ccb45393fa142f19
Peter Camps <peter.camps@ugent.be>
2025-01-11 13:31:06 +0100

    initial BoxSearch implementation (unused)

---
+++ SKIRT/utils/BoxSearch.cpp
@@ -0,0 +1,263 @@
+/*//////////////////////////////////////////////////////////////////
+////     The SKIRT project -- advanced radiative transfer       ////
+////       © Astronomical Observatory, Ghent University         ////
+///////////////////////////////////////////////////////////////// */
+
+#include "BoxSearch.hpp"
+#include "EntityCollection.hpp"
+#include "NR.hpp"
+
+////////////////////////////////////////////////////////////////////
+
+namespace
+{
+    // This function determines the grid block separation points along a specified axis 1->x, 2->y, 3->z
+    // to ensure that the entities are evenly distributed across the grid blocks.
+    // It does this by binning the bounding box centers at a high resolution and then calculating
+    // the cumulative distribution to set the grid separation points.
+    void makegrid(Array& grid, const vector<Box>& boxv, int axis, int gridsize, double cmin, double cmax)
+    {
+        // guard against degenerate zero-width entities that are all lined up along this coordinate
+        if (cmin == cmax)
+        {
+            double eps = 1e-12 * (cmin ? abs(cmin) : 1.);
+            cmin -= eps;
+            cmax += eps;
+        }
+
+        // determine the entity center distribution by binning at a decent resolution
+        int nbins = gridsize * 100;
+        double binwidth = (cmax - cmin) / nbins;
+        vector<int> bins(nbins);
+        for (const auto& box : boxv)
+        {
+            double center = 0.;
+            switch (axis)
+            {
+                case 1: center = box.center().x(); break;
+                case 2: center = box.center().y(); break;
+                case 3: center = box.center().z(); break;
+            }
+            bins[static_cast<int>((center - cmin) / binwidth)] += 1;
+        }
+
+        // determine grid separation points based on the cumulative distribution
+        grid.resize(gridsize + 1);
+        grid[0] = -std::numeric_limits<double>::infinity();
+        int perblock = boxv.size() / gridsize;  // target number of entities per block
+        int cumul = 0;                          // cumulative number of entities in processed bins
+        int gridindex = 1;                      // index of the next grid separation point to be filled
+        for (int binindex = 0; binindex < nbins; binindex++)
+        {
+            cumul += bins[binindex];
+            if (cumul > perblock * gridindex)
+            {
+                grid[gridindex] = cmin + (binindex + 1) * binwidth;
+                gridindex += 1;
+                if (gridindex >= gridsize) break;
+            }
+        }
+        grid[gridsize] = std::numeric_limits<double>::infinity();
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+
+BoxSearch::BoxSearch() {}
+
+////////////////////////////////////////////////////////////////////
+
+void BoxSearch::loadEntities(int numEntities, std::function<Box(int)> bounds,
+                             std::function<bool(int, const Box&)> intersects)
+{
+    // abort if there are no entities
+    if (numEntities <= 0)
+    {
+        _extent = Box();
+        _numBlocks = 0;
+        _minEntitiesPerBlock = 0;
+        _maxEntitiesPerBlock = 0;
+        _avgEntitiesPerBlock = 0;
+        return;
+    }
+
+    // cache the bounding boxes because we need them a few times
+    vector<Box> boxv;
+    boxv.reserve(numEntities);
+    for (int m = 0; m != numEntities; ++m) boxv.emplace_back(bounds(m));
+
+    // calculate the extent of the search domain
+    _extent = boxv[0];
+    for (const auto& box : boxv) _extent.extend(box);
+
+    // determine the number of blocks in each spatial direction;
+    // the floor of 20 is exceeded only for more than a million entities
+    _numBlocks = max(20, static_cast<int>(std::cbrt(numEntities) / 5.));
+
+    // build the grids in each spatial direction
+    makegrid(_xgrid, boxv, 1, _numBlocks, _extent.xmin(), _extent.xmax());
+    makegrid(_ygrid, boxv, 2, _numBlocks, _extent.ymin(), _extent.ymax());
+    makegrid(_zgrid, boxv, 3, _numBlocks, _extent.zmin(), _extent.zmax());
+
+    // construct an empty entity list for each of the nb*nb*nb blocks
+    _listv.clear();  // remove any pre-existing lists
+    _listv.resize(_numBlocks * _numBlocks * _numBlocks);
+
+    // add each entity to the list for every block that its bounding box overlaps
+    for (int m = 0; m != numEntities; ++m)
+    {
+        const auto& box = boxv[m];
+
+        // find indices for first and last grid block overlapped by bounding box, in each spatial direction
+        int i1 = NR::locateClip(_xgrid, box.xmin());
+        int i2 = NR::locateClip(_xgrid, box.xmax());
+        int j1 = NR::locateClip(_ygrid, box.ymin());
+        int j2 = NR::locateClip(_ygrid, box.ymax());
+        int k1 = NR::locateClip(_zgrid, box.zmin());
+        int k2 = NR::locateClip(_zgrid, box.zmax());
+
+        // loop over all blocks in that 3D range
+        for (int i = i1; i <= i2; i++)
+            for (int j = j1; j <= j2; j++)
+                for (int k = k1; k <= k2; k++)
+                {
+                    // add the entity to the list if it indeed overlaps the block
+                    Box block(_xgrid[i], _ygrid[j], _zgrid[k], _xgrid[i + 1], _ygrid[j + 1], _zgrid[k + 1]);
+                    if (intersects(m, block)) _listv[blockIndex(i, j, k)].push_back(m);
+                }
+    }
+
+    // calculate statistics
+    _minEntitiesPerBlock = numEntities;
+    _maxEntitiesPerBlock = 0;
+    int totalEntityReferences = 0;
+    for (const auto& list : _listv)
+    {
+        int size = list.size();
+        _minEntitiesPerBlock = min(_minEntitiesPerBlock, size);
+        _maxEntitiesPerBlock = max(_maxEntitiesPerBlock, size);
+        totalEntityReferences += size;
+    }
+    _avgEntitiesPerBlock = static_cast<double>(totalEntityReferences) / _listv.size();
+}
+
+////////////////////////////////////////////////////////////////////
+
+const Box& BoxSearch::extent() const
+{
+    return _extent;
+}
+
+////////////////////////////////////////////////////////////////////
+
+int BoxSearch::numBlocks() const
+{
+    return _numBlocks;
+}
+
+////////////////////////////////////////////////////////////////////
+
+int BoxSearch::minEntitiesPerBlock() const
+{
+    return _minEntitiesPerBlock;
+}
+
+////////////////////////////////////////////////////////////////////
+
+int BoxSearch::maxEntitiesPerBlock() const
+{
+    return _maxEntitiesPerBlock;
+}
+
+////////////////////////////////////////////////////////////////////
+
+int BoxSearch::avgEntitiesPerBlock() const
+{
+    return _avgEntitiesPerBlock;
+}
+
+////////////////////////////////////////////////////////////////////
+
+int BoxSearch::firstEntityContaining(Vec bfr, std::function<bool(int)> contains) const
+{
+    if (!_numBlocks) return -1;
+
+    // get the indices for the block containing the position
+    int i = NR::locateClip(_xgrid, bfr.x());
+    int j = NR::locateClip(_ygrid, bfr.y());
+    int k = NR::locateClip(_zgrid, bfr.z());
+
+    // loop over all entities overlapping that block; return the first one that contains the position
+    for (int m : _listv[blockIndex(i, j, k)])
+    {
+        if (contains(m)) return m;
+    }
+
+    // there is no entity containing the position
+    return -1;
+}
+
+////////////////////////////////////////////////////////////////////
+
+void BoxSearch::getEntities(EntityCollection& entities, Vec bfr, std::function<double(int)> weight) const
+{
+    entities.clear();
+    if (!_numBlocks) return;
+
+    // get the indices for the block containing the position
+    int i = NR::locateClip(_xgrid, bfr.x());
+    int j = NR::locateClip(_ygrid, bfr.y());
+    int k = NR::locateClip(_zgrid, bfr.z());
+
+    // add all entities overlapping that block to the collection with their respective weight
+    for (int m : _listv[blockIndex(i, j, k)])
+    {
+        entities.add(m, weight(m));
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+
+void BoxSearch::getEntities(EntityCollection& entities, Position bfr, Direction bfk,
+                            std::function<double(int)> weight) const
+{
+    entities.clear();
+    if (!_numBlocks) return;
+
+    // verify that the path intersects the domain
+    double smin, smax;
+    if (_extent.intersects(bfr, bfk, smin, smax))
+    {
+        // find the indices for first and last block, in each spatial direction,
+        // overlapped by the bounding box of the path's intersection with the domain
+        Box pathbox(bfr + smin * bfk, bfr + smax * bfk);
+        int i1 = NR::locateClip(_xgrid, pathbox.xmin());
+        int i2 = NR::locateClip(_xgrid, pathbox.xmax());
+        int j1 = NR::locateClip(_ygrid, pathbox.ymin());
+        int j2 = NR::locateClip(_ygrid, pathbox.ymax());
+        int k1 = NR::locateClip(_zgrid, pathbox.zmin());
+        int k2 = NR::locateClip(_zgrid, pathbox.zmax());
+        if (i1 > i2) std::swap(i1, i2);  // fix reversed coords for negative bfk components
+        if (j1 > j2) std::swap(j1, j2);
+        if (k1 > k2) std::swap(k1, k2);
+
+        // loop over all blocks in that 3D range
+        for (int i = i1; i <= i2; i++)
+            for (int j = j1; j <= j2; j++)
+                for (int k = k1; k <= k2; k++)
+                {
+                    // if the path intersects the block
+                    Box block(_xgrid[i], _ygrid[j], _zgrid[k], _xgrid[i + 1], _ygrid[j + 1], _zgrid[k + 1]);
+                    if (block.intersects(bfr, bfk, smin, smax))
+                    {
+                        // add all entities overlapping that block to the collection with their respective weight
+                        for (int m : _listv[blockIndex(i, j, k)])
+                        {
+                            entities.add(m, weight(m));
+                        }
+                    }
+                }
+    }
+}
+
+////////////////////////////////////////////////////////////////////
---
+++ SKIRT/utils/BoxSearch.hpp
@@ -0,0 +1,145 @@
+/*//////////////////////////////////////////////////////////////////
+////     The SKIRT project -- advanced radiative transfer       ////
+////       © Astronomical Observatory, Ghent University         ////
+///////////////////////////////////////////////////////////////// */
+
+#ifndef BOXSEARCH_HPP
+#define BOXSEARCH_HPP
+
+#include "Array.hpp"
+#include "Box.hpp"
+#include "Direction.hpp"
+#include "Position.hpp"
+#include <functional>
+class EntityCollection;
+
+//////////////////////////////////////////////////////////////////////
+
+/** BoxSearch is a helper class for organizing spatial objects in a data structure that allows
+    efficient retrieval of all objects that overlap a given point or ray. The class actually works
+    with the bounding boxes of the objects being held, and leaves more detailed tests for
+    containment or intersection to the client code.
+
+    The spatial objects held by a BoxSearch instance are called entities. They are identified by
+    a unique index \f$m\f$ ranging from 0 to \f$M-1\f$, where \f$M\f$ is the number of managed
+    entities.
+
+    To be completed. */
+class BoxSearch
+{
+    // ------- Constructing and loading -------
+
+public:
+    /** The constructor creates a trivial BoxSearch instance holding no entities. Any queries
+        will come up empty. */
+    BoxSearch();
+
+    /** This function loads the specified number of entities \f$M\f$ into the search structure,
+        using the information returned by the provided callback functions. Any entities held
+        prevously are removed and replaced by the new ones.
+
+        The \em bounds callback function returns the bounding box of the entity with the given
+        index. The \em intersects callback function returns true if the entity with the given index
+        possibly intersects the specified box, and false otherwise. A relaxed intersection test
+        such as testing against the bounding box is allowed but a stricter intersection test will
+        result in more efficient retrieval queries.
+
+        The callback functions are invoked one or more times for indices \f$m\f$ ranging from 0 to
+        \f$M-1\f$, in arbitrary order. For given values of their argument(s), the callback
+        functions must always return the same value. */
+    void loadEntities(int numEntities, std::function<Box(int m)> bounds,
+                      std::function<bool(int m, const Box& box)> intersects);
+
+    // ------- Getting properties and statistics -------
+
+public:
+    /** This function returns the extent of the search domain, i.e. the union of all entity
+        bounding boxes. */
+    const Box& extent() const;
+
+    /** This function returns the number of blocks in the search structure for each spatial
+        dimension. */
+    int numBlocks() const;
+
+    /** This function returns the smallest number of entity references in a search block. */
+    int minEntitiesPerBlock() const;
+
+    /** This function returns the largest number of entity references in a search block. */
+    int maxEntitiesPerBlock() const;
+
+    /** This function returns the average number of entity references in a search block. */
+    int avgEntitiesPerBlock() const;
+
+    // ------- Querying -------
+
+public:
+    /** This function returns the index \f$m\f$ of the first entity containing the specified
+        position, or -1 if there is no such entity. The callback function must return true if the
+        entity with given index contains the position passed to the main function, and false
+        otherwise. It is called zero or more times with the following guarantees: (1) all entities
+        whose bounding box overlaps the position are included in the invocation sequence, and (2)
+        the indices of consecutive invocations are sorted in increasing order.
+
+        The first index for which the callback function returns true is returned to the caller of
+        the main function. In case multiple entities overlap the position, because of the second
+        guarantee, the entity with the smallest index is always returned.
+
+        Note that the callback function may be called for entities whose bounding box does \em not
+        overlap the position; in that case it should return false. */
+    int firstEntityContaining(Vec bfr, std::function<bool(int m)> contains) const;
+
+    /** This function replaces the contents of the specified entity collection by the set of
+        entities that overlap the specified position with a nonzero weight. If there are no such
+        entities, the collection will be empty.
+
+        The callback function must return the weight in the entity with given index for the
+        position passed to the main function, or zero if the entity does not contain the position.
+        The callback function will be invoked with a sequence of indices in arbitrary order and
+        possibly with duplicates. For a given index, the callback function must always return the
+        same value. Even if the callback function is invoked multiple times for the same index, the
+        corresponding entity will be added to the entity collection just once.
+
+        Note that the callback function may be called for entities whose bounding box does \em not
+        overlap the position; in that case it should return zero. */
+    void getEntities(EntityCollection& entities, Vec bfr, std::function<double(int m)> weight) const;
+
+    /** This function replaces the contents of the specified entity collection by the set of
+        entities that overlap the specified ray (starting point and direction) with a nonzero
+        weight. If there are no such entities, the collection will be empty.
+
+        The callback function must return the weight in the entity with given index for the ray
+        passed to the main function, or zero if the entity does not intersect the ray. The callback
+        function will be invoked with a sequence of indices in arbitrary order and possibly with
+        duplicates. For a given index, the callback function must always return the same value.
+        Even if the callback function is invoked multiple times for the same index, the
+        corresponding entity will be added to the entity collection just once.
+
+        Note that the callback function may be called for entities whose bounding box does \em not
+        overlap the position; in that case it should return zero. */
+    void getEntities(EntityCollection& entities, Position bfr, Direction bfk,
+                     std::function<double(int m)> weight) const;
+
+    // ------- Private helper functions -------
+
+private:
+    /** This function returns the linear index for element (i,j,k) in the block list table. */
+    int blockIndex(int i, int j, int k) const { return ((i * _numBlocks) + j) * _numBlocks + k; }
+
+    // ------- Data members -------
+
+private:
+    // search structure
+    Box _extent;                   // the extent of the search domain, i.e. the union of all bounding boxes
+    int _numBlocks{0};             // the number of grid blocks nb in each direction
+    Array _xgrid, _ygrid, _zgrid;  // the nb+1 grid separation points for each spatial direction
+    vector<vector<int>> _listv;    // the nb*nb*nb lists of indices for entities overlapping each block
+
+    // statistics
+    int _minEntitiesPerBlock{0};
+    int _maxEntitiesPerBlock{0};
+    double _avgEntitiesPerBlock{0};
+};
+
+//////////////////////////////////////////////////////////////////////
+
+#endif
